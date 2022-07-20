4aafbcb81022d94c128da10bd0acc502d206679d
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-19 20:05:27 -0700

    Render GlobalStyles component

--- california-dev/src/index.js
+++ california-dev/src/index.js
@@ -10,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <App />
     </ThemeProvider>
   </React.StrictMode>
