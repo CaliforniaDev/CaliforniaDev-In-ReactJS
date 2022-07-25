3bd4e9b8dd371a92fa8afdeff31d3036f46428e9
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-25 00:51:06 -0700

    Add NormalizeStyles component to root

--- california-dev/src/index.js
+++ california-dev/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import GlobalStyles from "theme/GlobalStyles";
 import theme from "theme/theme";
 import { ThemeProvider } from "styled-components";
 import reportWebVitals from "./reportWebVitals";
+import {GlobalStyles, NormalizeStyles} from "theme/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <NormalizeStyles />
       <GlobalStyles />
       <App />
     </ThemeProvider>
