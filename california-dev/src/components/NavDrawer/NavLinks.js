b741dd016a51bf793d03a66a5b10684d0a79ffa3
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-25 00:38:29 -0700

    Fix list items not individually wrapped elements

--- california-dev/src/components/NavDrawer/NavLinks.js
+++ california-dev/src/components/NavDrawer/NavLinks.js
@@ -1,3 +1,4 @@
+import * as Styled from './styles';
 import homeIcon from "lib/assets/images/home_white_24dp.svg";
 import skillsIcon from "lib/assets/images/code_white_24dp.svg";
 import projectIcon from "lib/assets/images/media_white_24dp.svg";
@@ -5,14 +6,12 @@ import emailIcon from "lib/assets/images/email_white_24dp.svg";
 
 const NavLinks = () => {
   return (
-    <ul>
-      <li>
-        <img src={homeIcon} />
-        <img src={skillsIcon} />
-        <img src={projectIcon} />
-        <img src={emailIcon} /> 
-      </li>
-    </ul>
+    <Styled.NavLinks className='links'>
+        <li><img src={homeIcon} /></li>
+        <li><img src={skillsIcon} /></li>
+        <li><img src={projectIcon} /></li>
+        <li><img src={emailIcon} /> </li>
+    </Styled.NavLinks>
   );
 }
 
