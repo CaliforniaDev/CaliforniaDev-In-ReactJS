f5c6cfa1f73418020772b19b58ece3371767317f
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-24 23:05:44 -0700

    Create styled component for Nav Drawer

---
+++ california-dev/src/components/NavDrawer/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.nav`
+  z-index: 100;
+  margin: 0px;
+  padding: 0px;
+  position: fixed;
+  display: flex;
+  width: 100px;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 1.6rem 0;
+  max-width: 100%;
+  background-color: ${({theme}) => theme.color.primary};
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, .16);
+  transition: 0.3s;
+`
