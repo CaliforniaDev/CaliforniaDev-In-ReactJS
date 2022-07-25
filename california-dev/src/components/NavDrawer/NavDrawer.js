3d9aa7621a3905677d387a4f4fea31307be768c0
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-25 00:39:02 -0700

    Add id to NavLinks component

--- california-dev/src/components/NavDrawer/NavDrawer.js
+++ california-dev/src/components/NavDrawer/NavDrawer.js
@@ -1,15 +1,12 @@
-import * as Styled from './styles';
-import Logo from './Logo';
-import NavLinks from './NavLinks';
+import * as Styled from "./styles";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 export const NavDrawer = () => {
-
   return (
     <Styled.NavContainer id="NavDrawer">
       <Logo />
-      <NavLinks />
+      <NavLinks id="nav-links" />
     </Styled.NavContainer>
-  )
-}
-
-
+  );
+};
