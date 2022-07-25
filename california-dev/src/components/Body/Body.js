29f3583851941aa2d1264707b88c014d876ba6a7
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-25 16:48:15 -0700

    Add hero header and profile image content

--- california-dev/src/components/Body/Body.js
+++ california-dev/src/components/Body/Body.js
@@ -1,8 +1,15 @@
 import * as Styled from "./styles";
+import profileImg from "lib/assets/images/profile.JPG";
 export const Body = () => {
   return (
     <main id="main">
-      <Styled.HeroSection />
+      <Styled.HeroSection id="hero">
+        <Styled.HeroHeader className="hero__col-1">
+          <div className="profile-img-container">
+            <img class="profile-img" src={profileImg} />
+          </div>
+        </Styled.HeroHeader>
+      </Styled.HeroSection>
     </main>
   )
 }
