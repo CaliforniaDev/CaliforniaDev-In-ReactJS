bf51e3859aa2d99f1eed7e234b420f074972f087
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-25 02:00:01 -0700

    Import body components

--- california-dev/src/App.js
+++ california-dev/src/App.js
@@ -1,8 +1,10 @@
 import { NavDrawer } from "./components/NavDrawer";
+import { Body } from "./components/Body";
 function App() {
   return (
     <>
       <NavDrawer />
+      <Body />
     </>
   );
 }
