0cdb4ea443fb1345de05e067b2e7fe406c934621
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-25 16:47:54 -0700

    Create styled hero header

--- california-dev/src/components/Body/styles.js
+++ california-dev/src/components/Body/styles.js
@@ -9,3 +9,33 @@ export const HeroSection = styled.section`
   /* mobile viewport bug fix */
   min-height: -webkit-fill-available;
 `
+export const HeroHeader = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 1.6rem;
+  padding: 0 1.6rem;
+
+  p,span {
+    font-size: 2.4rem;
+    line-height: 3.2rem;
+  }
+
+  .profile-img-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    border-radius: 50%;
+    width: 350px;
+    min-width: 350px;
+    height: 350px;
+  }
+
+  .profile-img {
+    height: 475px;
+  }
+`
+export const HeroContent = styled.div`
+
+`
