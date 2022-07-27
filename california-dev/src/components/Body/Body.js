9fdc68372b3d870ff722b6522c4b04383609685f
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-26 19:41:25 -0700

    Extract hero from body component

--- california-dev/src/components/Body/Body.js
+++ california-dev/src/components/Body/Body.js
@@ -1,16 +1,10 @@
-import * as Styled from "./styles";
-import profileImg from "lib/assets/images/profile.JPG";
+import { Hero } from "./Hero";
+
+
 export const Body = () => {
   return (
     <main id="main">
-      <Styled.HeroSection id="hero">
-        <Styled.HeroHeader className="hero__col-1">
-          <div className="profile-img-container">
-            <img class="profile-img" src={profileImg} />
-          </div>
-        </Styled.HeroHeader>
-      </Styled.HeroSection>
+      <Hero />
     </main>
-  )
-}
-
+  );
+};
