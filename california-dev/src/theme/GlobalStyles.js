f25441b4e8bf5e69c21b816330b5a8801d15a32f
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-26 22:41:02 -0700

    Style

--- california-dev/src/theme/GlobalStyles.js
+++ california-dev/src/theme/GlobalStyles.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
+
 export const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Lato&family=Roboto:ital,wght@0,400;0,500;1,400;1,500&display=swap');
+
 html, body {
   font-size: 62.5%;
   line-height: 1.6;
@@ -13,7 +16,8 @@ html, body {
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
   scroll-behavior: smooth;
-  font-family: "Roboto Condensed";
+  font-family: 'Roboto', sans-serif;
+  background-color: ${({theme}) => theme.color.secondary};
 }
 *, *::before, *::after {
   padding: 0;
@@ -37,7 +41,40 @@ h6 {
   margin: 0;
   line-height: 1em;
 }
+
+span, label {
+  font-family: 'Roboto', sans-serif;
+}
+
+h1, h2, h3, h4 {
+  font-family: 'Lato', sans-serif;
+  font-weight: 400;
+}
+
+h1 {
+  font-size: 5.6rem;
+  line-height: 7.2rem;
+}
+
+h2 {
+  font-size: 4.0rem;
+  line-height: 5.2rem;
+}
+
+h3 {
+  font-size: 3.2rem;
+  line-height: 4rem;
+}
+
+h4 {
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+}
+span {
+  font-size: 1.6rem;
+}
 `;
+
 
 export const NormalizeStyles = createGlobalStyle`
 * 1. Change the font styles in all browsers.
