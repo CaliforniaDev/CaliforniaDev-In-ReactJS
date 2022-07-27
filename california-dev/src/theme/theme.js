56ace355edcd9adab0897e13564b02bdf72fdd72
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-27 00:32:20 -0700

    Add color styling theme

--- california-dev/src/theme/theme.js
+++ california-dev/src/theme/theme.js
@@ -1,7 +1,10 @@
 const theme = {
   color: {
     primary: "#006699",
+    onPrimary: "#fff",
     secondary: "#FFCC66",
+    onSecondary: "#444444",
+    onSecondaryVarient: "#006699",
     tertiary: "#FF6666",
   },
 };
