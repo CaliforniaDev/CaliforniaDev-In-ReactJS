8defb0317e556d3b508ce26d012697abbcf62a3e
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-26 22:41:33 -0700

    Remove hero section background color

--- california-dev/src/components/Body/Hero/styles.js
+++ california-dev/src/components/Body/Hero/styles.js
@@ -6,7 +6,6 @@ export const Section = styled.section`
   display: flex;
   justify-content: center;
   margin: auto 0;
-  background-color: red;
   min-height: 100vh;
   /* mobile viewport bug fix */
   min-height: -webkit-fill-available;
