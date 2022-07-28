981f6e288313a1fc96160331b7364b3a40571378
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-28 15:15:19 -0700

    Import skills section component to body

--- california-dev/src/components/Body/Body.js
+++ california-dev/src/components/Body/Body.js
@@ -1,10 +1,12 @@
 import { Hero } from "./Hero";
+import { SkillsSection } from "./SkillsSection.js";
 
 
 export const Body = () => {
   return (
     <main id="main">
       <Hero />
+      <SkillsSection />
     </main>
   );
 };
