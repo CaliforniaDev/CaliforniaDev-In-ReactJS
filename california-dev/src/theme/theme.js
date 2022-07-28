d1ce30624b8b5affcfa4567c2a7c4721ccbdf8e5
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-28 00:42:51 -0700

    Add constants to help with maintainability

--- california-dev/src/theme/theme.js
+++ california-dev/src/theme/theme.js
@@ -1,15 +1,34 @@
+/**
+ * * Change color swatches from these constants
+ */
+const primary = "#006699";
+const onPrimary = "#fff";
+
+const secondary = "#FFCC66";
+const onSecondary =  "#444444";
+const onSecondaryVarient = "#006699";
+
+const tertiary = "#FF6666";
+
+
 const theme = {
   color: {
-    primary: "#006699",
-    onPrimary: "#fff",
-    secondary: "#FFCC66",
-    onSecondary: "#444444",
-    onSecondaryVarient: "#006699",
-    tertiary: "#FF6666",
+    primary: primary,
+    onPrimary: onPrimary,
+    secondary: secondary,
+    onSecondary: onSecondary,
+    onSecondaryVarient: onSecondaryVarient,
+    tertiary: tertiary,
+
+    inverse: {
+      primary: onPrimary,
+      onPrimary: primary,
+    },
   },
+
   shadow: {
-    button: "0px 3px 6px rgba(0, 0, 0, 0.16)"
-  }
+    button: "0px 3px 6px rgba(0, 0, 0, 0.16)",
+  },
 };
 
 export default theme;
