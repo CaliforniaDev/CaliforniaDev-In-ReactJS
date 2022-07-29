ddeede4e34277905a6d448c31a4f5996663b5f26
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-28 18:10:01 -0700

    Add elevated card styled component

--- california-dev/src/components/Body/SkillsSection.js/styles.js
+++ california-dev/src/components/Body/SkillsSection.js/styles.js
@@ -11,3 +11,14 @@ export const Section = styled.section`
   background-color: ${({ theme }) => theme.color.tertiary};
   padding: 0 1.6rem;
 `;
+
+export const ElevatedCard = styled.div`
+  z-index: 1;
+  margin-top: -5rem; // Creates slight offset from section
+  border-radius: 1.6rem;
+  display: block;
+  height: auto;
+  width: 100%;
+  background-color: ${({theme}) => theme.color.tertiary};
+  box-shadow: ${({theme}) => theme.shadow.button};
+`;
