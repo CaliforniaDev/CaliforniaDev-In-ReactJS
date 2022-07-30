2985dad4464ef36c59ade742fe912cd67f551f1e
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-29 19:57:30 -0700

    Remove unecessary styling and inconsistencies

--- california-dev/src/components/Body/Hero/styles.js
+++ california-dev/src/components/Body/Hero/styles.js
@@ -19,17 +19,15 @@ export const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 0 1.6rem;
   padding: 0 1.6rem;
+  
 
   p {
     font-size: 2.4rem;
     line-height: 3.2rem;
   }
 
-  h2 {
-    margin-bottom: 1.6rem;
-  }
+ 
 
   .profile-img-container {
     display: flex;
