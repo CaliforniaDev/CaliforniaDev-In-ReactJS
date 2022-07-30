687fb04b5680a8a7072324087102b5a580a959c8
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-07-29 19:26:59 -0700

    Change class names

--- california-dev/src/components/Body/SkillsSection.js/SkillsSection.js
+++ california-dev/src/components/Body/SkillsSection.js/SkillsSection.js
@@ -1,16 +1,18 @@
 import * as Styled from "./styles";
+import penToolImg from "lib/assets/images/pen-tool.svg";
+import computerImg from "lib/assets/images/desktop-icon.svg";
 export const SkillsSection = () => {
   return (
-    <Styled.Section>
-      <Styled.ElevatedCard>
-        <div class="skills-container">
-          <div class="skills__col">
-            <div class="col__content">
-              <figure class="skills__img">
-                <img src="images/pen-tool.svg" />
+    <Styled.Section id="skills">
+      <Styled.ElevatedCard id="skills-card">
+        <div className="flex-container">
+          <div className="skills__col">
+            <div className="col__content">
+              <figure className="img-wrapper">
+                <img style={{height: "58px"}} src={penToolImg} alt="Graphic design pen tool icon"/>
               </figure>
-              <h4>UI / UX Designer</h4>
-              <p>
+              <h3>UI / UX Designer</h3>
+              <p style={{fontSize: "2rem"}}>
                 During the inital design process. I create both a low fidelity
                 wireframe, then an interactive hi-fidelity prototype using Adobe
                 XD. Each design goes through exstensive user testing to ensure
@@ -18,12 +20,12 @@ export const SkillsSection = () => {
               </p>
             </div>
           </div>
-          <div class="skills__col">
-            <div class="col__content">
-              <figure class="skills__img">
-                <img src="images/desktop-icon.svg" />
+          <div className="skills__col">
+            <div className="col__content">
+              <figure className="img-wrapper">
+                <img src={computerImg} alt="Desktop computer icon"/>
               </figure>
-              <h4>Front-end Developer</h4>
+              <h3>Front-end Developer</h3>
               <p>
                 After the client decides on a design, the next process is coming
                 up with the software architecture that best suits the project. I
@@ -40,4 +42,8 @@ export const SkillsSection = () => {
   );
 };
 
-const C = () => {};
+// const  SkillDescriptions = () => {
+//   return (
+
+//   )
+// };
