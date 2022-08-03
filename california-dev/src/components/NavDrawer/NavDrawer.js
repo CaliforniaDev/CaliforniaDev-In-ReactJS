b8660da4f7b172e714427723aeae474140539364
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-08-02 18:04:17 -0700

    Add scroll event effect

--- california-dev/src/components/NavDrawer/NavDrawer.js
+++ california-dev/src/components/NavDrawer/NavDrawer.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import * as Styled from "./styles";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 
 export const NavDrawer = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) return setIsVisible(true);
+      return setIsVisible(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <Styled.NavContainer id="NavDrawer">
+    <Styled.NavContainer id="NavDrawer" isVisible={isVisible}>
       <Logo />
       <NavLinks id="nav-links" />
     </Styled.NavContainer>
