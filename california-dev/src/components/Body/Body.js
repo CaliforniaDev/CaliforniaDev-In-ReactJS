02e5a924590048c1fe4ae521f5a9ae937d00da0a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-08-02 18:03:13 -0700

    Remove margin from body

--- california-dev/src/components/Body/Body.js
+++ california-dev/src/components/Body/Body.js
@@ -4,7 +4,7 @@ import { SkillsSection } from "./SkillsSection.js";
 
 export const Body = () => {
   return (
-    <main style={{marginLeft: "100px"}} id="main">
+    <main id="main">
       <Hero />
       <SkillsSection />
     </main>
