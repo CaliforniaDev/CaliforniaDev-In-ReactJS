372083ef86b1a742320e6b71d4b68942af046c61
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-08-03 17:30:35 -0700

    Remove scroll feature

--- california-dev/src/components/NavDrawer/NavDrawer.js
+++ california-dev/src/components/NavDrawer/NavDrawer.js
@@ -1,22 +1,10 @@
-import { useEffect, useState } from "react";
 import * as Styled from "./styles";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 
 export const NavDrawer = () => {
-  const [isVisible, setIsVisible] = useState(false);
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) return setIsVisible(true);
-      return setIsVisible(false);
-    };
-    window.addEventListener("scroll", handleScroll);
-    
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
-    <Styled.NavContainer id="NavDrawer" isVisible={isVisible}>
+    <Styled.NavContainer id="NavDrawer">
       <Logo />
       <NavLinks id="nav-links" />
     </Styled.NavContainer>
