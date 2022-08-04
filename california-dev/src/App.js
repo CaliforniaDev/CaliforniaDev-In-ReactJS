98c5186db9c0916b38920d0b14f28a2369fb7d7c
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-08-03 17:26:20 -0700

    Remove scroll event prop from component

--- california-dev/src/App.js
+++ california-dev/src/App.js
@@ -4,8 +4,8 @@ import { Body } from "./components/Body";
 function App() {
   return (
     <div>
-      <NavDrawer isScrollTop={isScrollTop} />
-      <Body isScrollTop={isScrollTop} />
+      <NavDrawer />
+      <Body />
     </div>
   );
 }
