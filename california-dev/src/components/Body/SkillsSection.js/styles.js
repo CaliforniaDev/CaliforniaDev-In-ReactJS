7cb083a3f557711bf8e235510c4e9e7f9789cc65
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-08-07 22:31:07 -0700

    Add styled component that holds skill tool icons and change flex box layout to column

--- california-dev/src/components/Body/SkillsSection.js/styles.js
+++ california-dev/src/components/Body/SkillsSection.js/styles.js
@@ -16,30 +16,60 @@ export const ElevatedCard = styled.div`
   z-index: 1;
   margin-top: -5rem; // Creates slight offset from section
   border-radius: 1.6rem;
-  display: block;
+  display: flex;
+  flex-direction: column;
   height: auto;
   width: 100%;
-  background-color: ${({theme}) => theme.color.tertiary};
-  box-shadow: ${({theme}) => theme.shadow.primary};
+  background-color: ${({ theme }) => theme.color.tertiary};
+  box-shadow: ${({ theme }) => theme.shadow.primary};
 
   .flex-container {
     display: flex;
-    justify-content: center;
+    position: relative;
+    align-items: center;
+    flex-direction: column;
     gap: 1.6rem;
     text-align: center;
-
+  }
+  .column {
+    flex-direction: column;
   }
   .skills__col {
-    flex-basis: 50%;
-   
+    flex-basis: 100%;
+
     padding: 1.6rem;
   }
   .img-wrapper {
     height: 6rem;
   }
-
   p {
     font-size: 2rem;
   }
+`;
 
+export const DevToolsContainer = styled.div`
+
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 1.6rem;
+  .tool-item {
+   
+    background-color: #f7f7f7;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border-radius: 0.8rem;
+    line-height: 3.2rem;
+    overflow: hidden;
+    width: 150px;
+    height: 150px;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.3);
+  }
+  .tools-col__content > img {
+    padding-top: 0.8rem;
+    width: 50%;
+    height: 50%;
+  }
 `;
