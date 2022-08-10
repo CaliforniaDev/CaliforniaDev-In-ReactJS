e30c3451b28f18cc63cbc082a1e2a53abc226cdd
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-08-09 19:32:54 -0700

    Refactor

--- california-dev/src/components/Body/SkillsSection.js/SkillsSection.js
+++ california-dev/src/components/Body/SkillsSection.js/SkillsSection.js
@@ -9,38 +9,37 @@ export const SkillsSection = () => {
       <Styled.ElevatedCard id="skills-card">
         <div className="flex-container">
           <div className="skills__col">
-              <figure className="img-wrapper">
-                <img
-                  style={{ height: "58px" }}
-                  src={penToolImg}
-                  alt="Graphic design pen tool icon"
-                />
-              </figure>
-              <h3>UI / UX Designer</h3>
-              <p>
-                During the inital design process. I create both a low fidelity
-                wireframe, then an interactive hi-fidelity prototype using Adobe
-                XD. Each design goes through exstensive user testing to ensure
-                the best possible user experience.
-              </p>
+            <figure className="img-wrapper">
+              <img
+                style={{ height: "58px" }}
+                src={penToolImg}
+                alt="Graphic design pen tool icon"
+              />
+            </figure>
+            <h3>UI / UX Designer</h3>
+            <p>
+              During the inital design process. I create both a low fidelity
+              wireframe, then an interactive hi-fidelity prototype using Adobe
+              XD. Each design goes through exstensive user testing to ensure the
+              best possible user experience.
+            </p>
           </div>
           <div className="skills__col">
-              <figure className="img-wrapper">
-                <img src={computerImg} alt="Desktop computer icon" />
-              </figure>
-              <h3>Front-end Developer</h3>
-              <p>
-                After the client decides on a design, the next process is coming
-                up with the software architecture that best suits the project. I
-                enjoy to write clean responsive code that renders well on a
-                variety of devices and windows. Each line of code is
-                thoughtfully written to increase performance and
-                maintainability.
-              </p>
+            <figure className="img-wrapper">
+              <img src={computerImg} alt="Desktop computer icon" />
+            </figure>
+            <h3>Front-end Developer</h3>
+            <p>
+              After the client decides on a design, the next process is coming
+              up with the software architecture that best suits the project. I
+              enjoy to write clean responsive code that renders well on a
+              variety of devices and windows. Each line of code is thoughtfully
+              written to increase performance and maintainability.
+            </p>
           </div>
         </div>
         <div className="flex-container column">
-           <h3>Skill-set & Tools</h3>
+          <h3>Skill-set & Tools</h3>
           <ToolItems />
         </div>
       </Styled.ElevatedCard>
