3e8c639baef5998dfe421eca5eaf1b38427f5962
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-08-19 01:02:22 -0700

    Add alt attributes to all images

--- california-dev/src/components/Body/SkillsSection.js/ToolItems.js
+++ california-dev/src/components/Body/SkillsSection.js/ToolItems.js
@@ -14,30 +14,37 @@ export const ToolItems = () => {
     {
       title: "HTML5",
       icon: html5Icon,
+      alt: "HTML5 logo image"
     },
     {
       title: "CSS3",
       icon: css3Icon,
+      alt: "CSS3 logo image"
     },
     {
       title: "JavaScript",
       icon: javaScriptIcon,
+      alt: "JavaScript logo image"
     },
     {
       title: "Adobe XD",
       icon: adobeXdIcon,
+      alt: "AdobeXD logo image"
     },
     {
       title: "illustrator",
       icon: illustratorIcon,
+      alt: "Adobe Illustrator logo image"
     },
     {
       title: "Photoshop",
       icon: photoshopIcon,
+      alt: "Adobe Photoshop logo image"
     },
     {
       title: "GitHub",
       icon: githubIcon,
+      alt: "Github logo image"
     },
   ];
   return (
@@ -46,7 +53,7 @@ export const ToolItems = () => {
           return (
             <li key={uuid()}>
               <figure className="tool-item flex-wrapper">
-                <img src={toolItem.icon} />
+                <img src={toolItem.icon} alt={toolItem.alt} />
                 <figcaption>{toolItem.title}</figcaption>
               </figure>
             </li>
