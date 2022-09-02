38832223c4199395cb90987d78411e5ad14821b6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-09-02 01:55:34 -0700

    Refactor styling

--- california-dev/src/components/Body/SkillsSection.js/styles.js
+++ california-dev/src/components/Body/SkillsSection.js/styles.js
@@ -3,18 +3,21 @@ import styled from "styled-components";
  * ! SKILLS SECTION STYLED COMPONENTS
  */
 export const Section = styled.section`
+  flex-direction: column;
   z-index: -1;
   position: relative;
   display: flex;
   justify-content: center;
   z-index: 1;
-  background-color: ${({ theme }) => theme.color.tertiary};
   padding: 0 1.6rem;
+  gap: 1.6rem;
 `;
-
+/**
+ * ! FIX  MARGIN TOP IF CHANGING BACK TO ORGINAL
+ */
 export const ElevatedCard = styled.div`
   z-index: 1;
-  margin-top: -5rem; // Creates slight offset from section
+  // margin-top: -5rem; // Creates slight offset from section 
   border-radius: 1.6rem;
   display: flex;
   flex-direction: column;
@@ -28,48 +31,49 @@ export const ElevatedCard = styled.div`
   .flex-container {
     display: flex;
     position: relative;
-    align-items: center;
     flex-direction: column;
     gap: 2.4rem;
-    text-align: center;
+    align-items: center;
+  
+    
   }
   .column {
     flex-direction: column;
   }
-  .skills__col {
-    flex-basis: 100%;
-  }
   .img-wrapper {
     height: 6rem;
   }
-  p {
-    font-size: 2rem;
+  .skills__col {
+    display: inline-flex;
+    align-items: center;
+    gap: 2.4rem;
   }
+
+
 `;
 
-export const DevToolsContainer = styled.div`
-
-    display: flex;
+export const DevToolsContainer = styled.ul`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 150px);
     justify-content: center;
-    flex-wrap: wrap;
+    list-style: none;
+    width: 100%;
     gap: 2.4rem;
+
   .tool-item {
-   
-    background-color: #f7f7f7;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
     border-radius: 0.8rem;
-    line-height: 3.2rem;
-    overflow: hidden;
     width: 150px;
     height: 150px;
     box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.3);
+    background-color: ${({theme}) => theme.color.onPrimary};
   }
-  .tools-col__content > img {
-    padding-top: 0.8rem;
-    width: 50%;
-    height: 50%;
+
+  .flex-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
   }
+ 
 `;
