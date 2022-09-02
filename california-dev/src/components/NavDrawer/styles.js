7586a1bdca8704d8980e11d6188c9ba9e89bc2c0
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-09-02 01:56:13 -0700

    Test overlay

--- california-dev/src/components/NavDrawer/styles.js
+++ california-dev/src/components/NavDrawer/styles.js
@@ -12,11 +12,19 @@ export const NavContainer = styled.nav`
   flex-direction: column;
   padding: 1.6rem 0;
   max-width: 100%;
-  background-color: ${({theme}) =>theme.color.primary};
+  background: #001F24;
+  // background-color: ${({theme}) =>theme.color.primary};
   box-shadow: 6px 0px 8px rgba(0, 0, 0, .3);
   transition: 0.2s;
   
 
+`
+export const Overlay = styled.div`
+  background: #9FCAFF;
+  opacity: 5%;
+  width: 100%;
+  height: 100%;
+  position: absolute;
 `
 
 export const NavLinks = styled.ul`
