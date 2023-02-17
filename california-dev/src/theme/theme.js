62c5e15e54455212d9260f10af3cb79dc1979496
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-02-17 02:08:47 -0800

    Format code

--- california-dev/src/theme/theme.js
+++ california-dev/src/theme/theme.js
@@ -1,36 +1,36 @@
 const theme = {
   lightMode: {
     palette: {
-      primary: '#0061A5',
-      onPrimary: '#ffffff',
-      primaryContainer: '#D2E4FF',
-      onPrimaryContainer: '#001D36',
+      primary: "#0061A5",
+      onPrimary: "#ffffff",
+      primaryContainer: "#D2E4FF",
+      onPrimaryContainer: "#001D36",
 
-      secondary: '#B8006D',
-      onSecondary: '#FFFFFF',
-      secondaryContainer: '#FFD9E4',
-      onSecondaryContainer: '#3E0021',
+      secondary: "#B8006D",
+      onSecondary: "#FFFFFF",
+      secondaryContainer: "#FFD9E4",
+      onSecondaryContainer: "#3E0021",
 
-      tertiary: '#6B5778',
-      onTertiary: '#FFFFFF',
-      tertiaryContainer: '#F3DAFF',
-      onTertiaryContainer: '#251431',
-      
-      error: '#BA1A1A',
-      onError: '#FFFFFF',
-      errorContainer: '#FFDAD6',
-      onErrorContainer: '#410002',
+      tertiary: "#6B5778",
+      onTertiary: "#FFFFFF",
+      tertiaryContainer: "#F3DAFF",
+      onTertiaryContainer: "#251431",
 
-      background: '#FDFCFF',
-      onBackground: '#1A1C1E',
-      surface: '#FDFCFF',
-      onSurface: '#1A1C1E',
+      error: "#BA1A1A",
+      onError: "#FFFFFF",
+      errorContainer: "#FFDAD6",
+      onErrorContainer: "#410002",
 
-      surfaceVariant: '#DFE2EB',
-      onSurfaceVariant: '#43474E',
-      outline: '#43474E',
-    }
-  } 
+      background: "#FDFCFF",
+      onBackground: "#1A1C1E",
+      surface: "#FDFCFF",
+      onSurface: "#1A1C1E",
+
+      surfaceVariant: "#DFE2EB",
+      onSurfaceVariant: "#43474E",
+      outline: "#43474E",
+    },
+  },
 };
 
 console.log(theme);
