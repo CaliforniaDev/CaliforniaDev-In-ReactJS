3c97c59c80f50790c8b2ecd20fe77ec07b2dd412
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-02-18 18:34:32 -0800

    Add dark mode color palette to theme

--- california-dev/src/theme/theme.js
+++ california-dev/src/theme/theme.js
@@ -31,6 +31,34 @@ const theme = {
       outline: "#43474E",
     },
   },
+
+  darkMode: {
+    palette: {
+      primary: "#9FCAFF",
+      onPrimary: "#003259",
+      primaryContainer: "#00497E",
+      onPrimaryContainer: "#D2E4FF",
+
+      secondary: "#FFB0CD",
+      onSecondary: "#640039",
+      secondaryContainer: "#8D0052",
+      onSecondaryContainer: "#FFD9E4",
+
+      tertiary: "#D7BEE4",
+      onTertiary: "#3B2947",
+      tertiaryContainer: "#523F5F",
+      onTertiaryContainer: "#F3DAFF",
+
+      background: "#1A1C1E",
+      onBackground: "#E2E2E6",
+      surface: "#1A1C1E",
+      onSurface: "#E2E2E6",
+
+      surfaceVariant: "#43474E",
+      onSurfaceVariant: "#C3C6CF",
+      outline: "#8D9199",
+    },
+  },
 };
 
 export default theme;
