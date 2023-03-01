5acd703be61445bff4201e1e4510092020eaaf69
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-02-28 20:05:18 -0800

    Add theming and local storage for user preference
    Refactor the App component to use a theming system and local storage for persisting the user's theme preference. Also, add a toggle button for switching between light and dark themes.
    
    Update imports to include the Theme component from the 'themes' library.
    
    This feature improves the user experience by allowing them to choose their preferred theme and saving their preference across browser sessions.

--- california-dev/src/App.js
+++ california-dev/src/App.js
@@ -1,10 +1,26 @@
 import { useState, useEffect } from "react";
+import { Theme } from "themes";
 import { Body } from "./components/Body";
 function App() {
+  // Set the initial theme preference based on the value stored in local storage
+  // or default to 'light' if no preference is found.
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") === "dark" ? "dark" : "light"
+  );
+
+  useEffect(() => {
+    // Save the user's theme preference to local storage whenever the theme changes
+    // to ensure that it persists across browser sessions.
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => setTheme(theme === "dark" ? "light" : "dark");
+
   return (
-    <div>
+    <Theme theme={theme}>
       <Body />
-    </div>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </Theme>
   );
 }
 
