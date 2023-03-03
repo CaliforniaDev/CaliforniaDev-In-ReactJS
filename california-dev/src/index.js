8e93dee2b535d3627c28b1fd30fbb52e7bef14c4
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-03-03 04:51:06 -0800

    feat: add ErrorBoundary component and import to index.js
    This commit adds an `ErrorBoundary` component to the project to catch and display any errors that may occur during rendering. The `ErrorBoundary` is located in a new `components` folder.
    
    To import the `ErrorBoundary` component into the application, I've added an import statement to `index.js`.
    
    These changes should improve the user experience by providing more informative error messages and preventing the application from crashing due to errors.

--- california-dev/src/index.js
+++ california-dev/src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import ErrorBoundary from "./ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
