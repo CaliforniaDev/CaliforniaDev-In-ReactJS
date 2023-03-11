de6f845482dac10abd9c7d116e5419a5e734569d
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-03-10 18:25:49 -0800

    refactor: use css helper for GlobalStyles component
    This commit refactors the GlobalStyles component to use the `css` helper
    to define the styles instead of a template literal. This provides more flexibility
    in terms of props and dynamic styles, and separates the styles from the component
    definition, making it more readable and easier to maintain. Additionally, this change
    follows the best practices for using `styled-components` and provides better performance
    optimizations by avoiding unnecessary re-renders of the component when only the props change.
    Overall, this change improves the maintainability and flexibility of the codebase.

--- california-dev/src/themes/GlobalStyles.js
+++ california-dev/src/themes/GlobalStyles.js
@@ -1,70 +1,71 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle((props) => {
   const { palette } = props.theme;
-  const {preferredColorScheme} = props;
-  return ` 
-  html, body {
-    font-size: 62.5%;
-    line-height: 1.5;
-    min-height: 100vh;
-    min-height: -webkit-fill-available;
-    height: -webkit-fill-available;
-    width: 100%;
-    box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    scroll-behavior: smooth;
-    font-family: 'Roboto', sans-serif;
-    background-color: ${palette.background};
-    color: ${palette.onBackground};
-    color-scheme: ${preferredColorScheme};
-    
-  }
-  
-  body, textarea {
-    font-size: 1.6rem;
-  }
-  
-  span, label {
-    font-family: 'Roboto', sans-serif;
-  }
-  ul {
-  
-    font-size: 1.6rem;
-  }
-  h1, h2, h3, h4 {
-    font-family: 'Lato', sans-serif;
-    font-weight: 400;
-  }
-  
-  h1 {
-    font-size: 5.6rem;
-  }
-  
-  h2 {
-    font-size: 4.0rem;
-  
-  }
-  
-  h3 {
-    font-size: 3.2rem;
-  
-  }
-  
-  h4 {
-    font-size: 2.4rem;
-  
-  }
-  
-  span {
-    font-size: inherit
-    line-height: inherit;
-  }
-  
-  p {
-    max-width: 75ch;
-  }
+  const { preferredColorScheme } = props;
+  return css`
+    html,
+    body {
+      font-size: 62.5%;
+      line-height: 1.5;
+      min-height: 100vh;
+      min-height: -webkit-fill-available;
+      height: -webkit-fill-available;
+      width: 100%;
+      box-sizing: border-box;
+      -webkit-box-sizing: border-box;
+      -moz-box-sizing: border-box;
+      scroll-behavior: smooth;
+      font-family: "Roboto", sans-serif;
+      background-color: ${palette.background};
+      color: ${palette.onBackground};
+      color-scheme: ${preferredColorScheme};
+    }
+
+    body,
+    textarea {
+      font-size: 1.6rem;
+    }
+
+    span,
+    label {
+      font-family: "Roboto", sans-serif;
+    }
+    ul {
+      font-size: 1.6rem;
+    }
+    h1,
+    h2,
+    h3,
+    h4 {
+      font-family: "Lato", sans-serif;
+      font-weight: 400;
+    }
+
+    h1 {
+      font-size: 5.6rem;
+    }
+
+    h2 {
+      font-size: 4rem;
+    }
+
+    h3 {
+      font-size: 3.2rem;
+    }
+
+    h4 {
+      font-size: 2.4rem;
+    }
+
+    span {
+      font-size: inherit;
+      line-height: inherit;
+    }
+
+    p {
+      max-width: 75ch;
+    }
   `;
 });
 
