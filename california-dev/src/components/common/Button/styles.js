08474528ce6ea50d47529d9c9710e3327b5e2fb2
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-03-10 18:37:27 -0800

    Refactor styled components using object destructuring in function parameters
    To avoid repeating the same code in each styled component, this commit refactors the code to use object destructuring in the function parameters. By directly accessing the `palette` and `typography` objects from the `theme` object, we can make the code more concise and easier to read, while reducing potential errors that can arise from copy-pasting code.

--- california-dev/src/components/common/Button/styles.js
+++ california-dev/src/components/common/Button/styles.js
@@ -1,73 +1,59 @@
 import styled, { css } from "styled-components";
 
-export const DefaultBtn = styled.button((props) => {
-  const {
-    theme: { palette },
-  } = props;
-  const {
-    theme: { typography },
-  } = props;
-  return css`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    padding: 1rem 2.4rem;
-    gap: 0.8rem;
-    height: 4rem;
-    outline-color: transparent;
-    border: none;
-    border-radius: 100px;
-    color: ${palette.onTertiary};
-    background: ${palette.tertiary};
+export const DefaultBtn = styled.button(
+  ({ theme: { palette, typography } }) => {
+    return css`
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      padding: 1rem 2.4rem;
+      gap: 0.8rem;
+      height: 4rem;
+      outline-color: transparent;
+      border: none;
+      border-radius: 100px;
+      color: ${palette.onTertiary};
+      background: ${palette.tertiary};
 
-    // Add typography styles to all buttons
-    ${typography.label.large}
+      // Add typography styles to all buttons
+      ${typography.label.large}
 
-    &:hover {
-      cursor: pointer;
-    }
-  `;
-});
+      &:hover {
+        cursor: pointer;
+      }
+    `;
+  }
+);
 
-export const ElevatedBtn = styled(DefaultBtn)((props) => {
-  const {
-    theme: { palette },
-  } = props;
-  const {
-    theme: { elevation },
-  } = props;
-  return css`
-    color: ${palette.onSurface};
-    background: ${elevation.level1.surfaceColor};
-    box-shadow: ${elevation.level1.shadow};
-  `;
-});
+export const ElevatedBtn = styled(DefaultBtn)(
+  ({ theme: { palette, elevation } }) => {
+    return css`
+      color: ${palette.onSurface};
+      background: ${elevation.level1.surfaceColor};
+      box-shadow: ${elevation.level1.shadow};
+    `;
+  }
+);
 
-export const FilledBtn = styled(DefaultBtn)((props) => {
-  const {
-    theme: { palette },
-  } = props;
+export const FilledBtn = styled(DefaultBtn)(
+  ({ theme: { palette } }) => {
   return css`
     background: ${palette.primary};
     color: ${palette.onPrimary};
   `;
 });
 
-export const TonalBtn = styled(DefaultBtn)((props) => {
-  const {
-    theme: { palette },
-  } = props;
+export const TonalBtn = styled(DefaultBtn)(
+  ({ theme: { palette } }) => {
   return css`
     background: ${palette.secondaryContainer};
     color: ${palette.onSecondaryContainer};
   `;
 });
 
-export const OutlinedBtn = styled(DefaultBtn)((props) => {
-  const {
-    theme: { palette },
-  } = props;
+export const OutlinedBtn = styled(DefaultBtn)(
+  ({ theme: { palette } }) => {
   return css`
     background: transparent;
     border-width: 0.1rem;
