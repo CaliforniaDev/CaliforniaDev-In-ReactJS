f5acd37e672aafb4f12f4564a81b654b9716c2f7
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-03-10 18:15:48 -0800

    refactor: use css helper to define styles for buttons
    This commit refactors the button components to use the `css` helper and a function
    to define the styles, providing more flexibility in terms of props and dynamic styles.
    Additionally, using the `css` helper separates the styles from the component definition,
    making it more readable and easier to maintain. Finally, this change provides better
    performance optimizations by avoiding unnecessary re-renders of the components when only
    the props change. Overall, this change follows the best practices for using `styled-components`
    and improves the flexibility and maintainability of the codebase.

--- california-dev/src/components/common/Button/styles.js
+++ california-dev/src/components/common/Button/styles.js
@@ -1,47 +1,78 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const DefaultBtn = styled.button`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem 2.4rem;
-  gap: 0.8rem;
-  height: 4rem;
-  outline-color: transparent;
-  border: none;
-  border-radius: 100px;
-  color: ${({ theme: { palette } }) => palette.onTertiary};
-  background: ${({ theme: { palette } }) => palette.tertiary};
-  
-  // Add typography styles to all buttons
-  ${({ theme: {typography} }) => typography.label.large};
+export const DefaultBtn = styled.button((props) => {
+  const {
+    theme: { palette },
+  } = props;
+  const {
+    theme: { typography },
+  } = props;
+  return css`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem 2.4rem;
+    gap: 0.8rem;
+    height: 4rem;
+    outline-color: transparent;
+    border: none;
+    border-radius: 100px;
+    color: ${palette.onTertiary};
+    background: ${palette.tertiary};
 
-  &:hover {
-    cursor: pointer;
-  }
-`;
+    // Add typography styles to all buttons
+    ${typography.label.large}
 
-export const ElevatedBtn = styled(DefaultBtn)`
-  color: ${({ theme }) => theme.palette.onSurface};
-  background: ${({ theme: { elevation } }) => elevation.level1.surfaceColor};
-  box-shadow: ${({ theme: { elevation } }) => elevation.level1.shadow};
-`;
+    &:hover {
+      cursor: pointer;
+    }
+  `;
+});
 
-export const FilledBtn = styled(DefaultBtn)`
-  background: ${({ theme }) => theme.palette.primary};
-  color: ${({ theme }) => theme.palette.onPrimary};
-`;
+export const ElevatedBtn = styled(DefaultBtn)((props) => {
+  const {
+    theme: { palette },
+  } = props;
+  const {
+    theme: { elevation },
+  } = props;
+  return css`
+    color: ${palette.onSurface};
+    background: ${elevation.level1.surfaceColor};
+    box-shadow: ${elevation.level1.shadow};
+  `;
+});
 
-export const TonalBtn = styled(DefaultBtn)`
-  background: ${({ theme: { palette } }) => palette.secondaryContainer};
-  color: ${({ theme: { palette } }) => palette.onSecondaryContainer};
-`;
+export const FilledBtn = styled(DefaultBtn)((props) => {
+  const {
+    theme: { palette },
+  } = props;
+  return css`
+    background: ${palette.primary};
+    color: ${palette.onPrimary};
+  `;
+});
 
-export const OutlinedBtn = styled(DefaultBtn)`
-  background: transparent;
-  border-width: 1px;
-  border-style: solid;
-  color: ${({ theme: { palette } }) => palette.primary};
-  border-color: ${({ theme: { palette } }) => palette.outline};
-`;
+export const TonalBtn = styled(DefaultBtn)((props) => {
+  const {
+    theme: { palette },
+  } = props;
+  return css`
+    background: ${palette.secondaryContainer};
+    color: ${palette.onSecondaryContainer};
+  `;
+});
+
+export const OutlinedBtn = styled(DefaultBtn)((props) => {
+  const {
+    theme: { palette },
+  } = props;
+  return css`
+    background: transparent;
+    border-width: 0.1rem;
+    border-style: solid;
+    color: ${palette.primary};
+    border-color: ${palette.outline};
+  `;
+});
