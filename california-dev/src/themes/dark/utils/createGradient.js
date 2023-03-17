abf51eb69f7e9219930055517574125dee6746e7
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-03-17 01:55:03 -0700

    Remove helper to reflect changes to to the elevations.js file

--- california-dev/src/themes/dark/utils/createGradient.js
+++
@@ -1,4 +0,0 @@
-import { palette } from "../palette";
-export const createGradient = (opacity1, opacity2) => {
-  return `linear-gradient(0deg, rgba(159, 202, 255, ${opacity1}), rgba(159, 202, 255, ${opacity2})), ${palette.surface}`;
-};
