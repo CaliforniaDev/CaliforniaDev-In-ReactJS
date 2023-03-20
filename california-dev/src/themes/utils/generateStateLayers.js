62e1fc6ab8d8aa9b17e86eae6dae0c9298ad01d7
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-03-20 06:49:00 -0700

    feat: Add CSS overlay generator function to state layer generation
    This commit updates the `generateStateLayers` function to take in a new parameter, `cssOverlay`, which generates the CSS overlay for each state layer value. This allows for better separation of concerns and improved maintainability of the code. Additionally, this commit updates the variable naming convention for state types to use camelCase instead of snake_case for better readability and consistency.
    
    The `generateStateLayers` function has been updated to include the new `cssOverlay` parameter, and the old `STATE` object has been replaced with `stateTypes` for consistency with the new naming convention. The `opacity` object has been left unchanged.
    
    As part of these changes, the file name for the state configuration object has been updated from `state-layers-config.js` to `state-config.js` for better readability and consistency.

--- california-dev/src/themes/utils/generateStateLayers.js
+++ california-dev/src/themes/utils/generateStateLayers.js
@@ -1,25 +1,43 @@
 import { hexToRgba } from "./hexToRgba";
 
-
 /**
  * Generates state layers for a given color palette.
  * @param {Object} palette - The color palette object.
  * @param {Object} stateTypes - The object defining different state types.
  * @param {Object} opacity - The object defining opacity values for each state type.
+ * @param {Function} cssOverlay - The CSS overlay generator function.
  * @returns {Object} - The state layers object containing state layers for each color in the palette.
  */
 
-function generateStateLayers(palette, STATE, OPACITY) {
-  const stateTypes = Object.values(STATE);
-  return Object.entries(palette).reduce((stateLayers, [colorRole, colorValue]) => {
-    const colorStateLayers = stateTypes.reduce((colorLayers, stateType) => {
-      const opacity = OPACITY[stateType];
-      colorLayers[stateType] = hexToRgba(colorValue, opacity);
-      return colorLayers;
-    }, {});
-    stateLayers[colorRole] = colorStateLayers;
-    return stateLayers;
-  }, {});
-}
+function generateStateLayers(palette, stateTypes, opacity, cssOverlay) {
+  // Get an array of the state types
+  const stateTypeList = Object.values(stateTypes);
 
+  // Iterate over each color in the palette
+  return Object.entries(palette).reduce(
+    (stateLayers, [colorRole, colorValue]) => {
+      // Generate state layers for each state type for this color
+      const colorStateLayers = stateTypeList.reduce(
+        (colorLayers, stateType) => {
+          // Get the opacity for this state type
+          const stateTypeOpacity = opacity[stateType];
+
+          // Generate the CSS overlay for this state layer value
+          const stateLayerValue = hexToRgba(colorValue, stateTypeOpacity);
+          const cssRule = cssOverlay(stateLayerValue);
+
+          // Add the CSS rule to the state layers object
+          colorLayers[stateType] = cssRule;
+          return colorLayers;
+        },
+        {}
+      );
+
+      // Add the state layers for this color to the state layers object
+      stateLayers[colorRole] = colorStateLayers;
+      return stateLayers;
+    },
+    {}
+  );
+}
 export { generateStateLayers };
