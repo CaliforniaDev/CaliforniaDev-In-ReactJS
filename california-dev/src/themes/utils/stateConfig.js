112d662329be82e886d737b861e803b33ff76c35
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-03-20 06:47:54 -0700

    feat: Add generateCssOverlay function and update STATE_TYPE variable naming convention

--- california-dev/src/themes/utils/stateConfig.js
+++ california-dev/src/themes/utils/stateConfig.js
@@ -1,5 +1,6 @@
+import { css } from "styled-components";
 // Define the different state types
-export const STATE = {
+export const STATE_TYPE = {
   HOVER: "hover",
   FOCUS: "focus",
   PRESSED: "pressed",
@@ -8,8 +9,29 @@ export const STATE = {
 
 // Define the opacity values for each state type
 export const OPACITY = {
-  [STATE.HOVER]: 0.08,
-  [STATE.FOCUS]: 0.12,
-  [STATE.PRESSED]: 0.12,
-  [STATE.DRAGGED]: 0.16,
+  [STATE_TYPE.HOVER]: 0.08,
+  [STATE_TYPE.FOCUS]: 0.12,
+  [STATE_TYPE.PRESSED]: 0.12,
+  [STATE_TYPE.DRAGGED]: 0.16,
+};
+
+/**
+ * Generate a CSS overlay for a given state layer value.
+ * @param {string} stateLayerValue - The value for the state layer.
+ * @returns {Object} - The CSS rule for the overlay.
+ */
+export const generateCssOverlay = (stateLayerValue) => {
+  return css`
+    ::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: ${stateLayerValue};
+      border-radius: inherit;
+      z-index: -1;
+    }
+  `;
 };
