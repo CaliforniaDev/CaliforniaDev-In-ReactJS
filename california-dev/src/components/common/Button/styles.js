982d17cb526c358b96cb0a80b1d67335910699ec
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-03-23 01:37:33 -0700

    feat: Update button styles with state and elevation transitions
    - Update DefaultBtn with transition and position properties
    - Add state and elevation transitions to ElevatedBtn, FilledBtn, TonalBtn, and OutlinedBtn

--- california-dev/src/components/common/Button/styles.js
+++ california-dev/src/components/common/Button/styles.js
@@ -1,7 +1,7 @@
 import styled, { css } from "styled-components";
 
 export const DefaultBtn = styled.button(
-  ({ theme: { palette, typography, elevation } }) => {
+  ({ theme: { palette, typography } }) => {
     return css`
       display: flex;
       flex-direction: row;
@@ -15,6 +15,9 @@ export const DefaultBtn = styled.button(
       border-radius: 100px;
       color: ${palette.onTertiary};
       background: ${palette.tertiary};
+      transition: box-shadow 0.2s ease-out;
+      position: relative;
+      z-index: 1;
 
       // Add typography styles to all buttons
       ${typography.label.large}
@@ -22,39 +25,108 @@ export const DefaultBtn = styled.button(
       &:hover {
         cursor: pointer;
       }
+      &:focus-visible {
+        outline: none;
+      }
+
+      margin-bottom: 1.6rem;
     `;
   }
 );
 
 export const ElevatedBtn = styled(DefaultBtn)(
-  ({ theme: { palette, elevation } }) => {
+  ({ theme: { palette, elevation, state } }) => {
+    console.log(elevation);
     return css`
-      ${elevation.level1};
-      color: ${palette.onSurface};
+      color: ${palette.primary};
+      ${elevation.surface.level1};
+      ${elevation.shadow.level1};
+
+      &:hover {
+        ${state.primary.hover};
+        ${elevation.surface.level2};
+        ${elevation.shadow.level2};
+      }
+      &:focus-visible {
+        ${state.primary.focus};
+        ${elevation.surface.level1};
+        ${elevation.shadow.level1};
+        outline: none;
+      }
+      &:active {
+        ${state.primary.pressed};
+        ${elevation.surface.level1};
+        ${elevation.shadow.level1};
+      }
     `;
   }
 );
 
-export const FilledBtn = styled(DefaultBtn)(({ theme: { palette } }) => {
-  return css`
-    background: ${palette.primary};
-    color: ${palette.onPrimary};
-  `;
-});
+export const FilledBtn = styled(DefaultBtn)(
+  ({ theme: { palette, elevation, state } }) => {
+    return css`
+      color: ${palette.onPrimary};
+      background: ${palette.primary};
+      ${elevation.shadow.level0};
 
-export const TonalBtn = styled(DefaultBtn)(({ theme: { palette } }) => {
-  return css`
-    background: ${palette.secondaryContainer};
-    color: ${palette.onSecondaryContainer};
-  `;
-});
+      &:hover {
+        ${state.onPrimary.hover};
+        ${elevation.shadow.level1};
+      }
+      &:focus-visible {
+        ${state.onPrimary.focus};
+        ${elevation.shadow.level0};
+      }
+      &:active {
+        ${state.onPrimary.pressed};
+        ${elevation.shadow.level0};
+      }
+    `;
+  }
+);
 
-export const OutlinedBtn = styled(DefaultBtn)(({ theme: { palette } }) => {
-  return css`
-    background: transparent;
-    border-width: 0.1rem;
-    border-style: solid;
-    color: ${palette.primary};
-    border-color: ${palette.outline};
-  `;
-});
+export const TonalBtn = styled(DefaultBtn)(
+  ({ theme: { palette, elevation, state } }) => {
+    return css`
+      color: ${palette.onSecondaryContainer};
+      background: ${palette.secondaryContainer};
+      ${elevation.shadow.level0};
+      &:hover {
+        ${state.onSecondaryContainer.hover};
+        ${elevation.shadow.level1};
+      }
+      &:focus-visible {
+        ${state.onSecondaryContainer.focus};
+        ${elevation.shadow.level0};
+      }
+      &:active {
+        ${state.onSecondaryContainer.pressed};
+        ${elevation.shadow.level0};
+      }
+    `;
+  }
+);
+
+export const OutlinedBtn = styled(DefaultBtn)(
+  ({ theme: { palette, elevation, state } }) => {
+    return css`
+      background: ${palette.surface};
+      border-width: 0.1rem;
+      border-style: solid;
+      color: ${palette.primary};
+      border-color: ${palette.outline};
+      ${elevation.level0};
+      &:hover {
+        ${state.primary.hover}
+      }
+      &:focus-visible {
+        border-color: ${palette.primary};
+        ${state.primary.focus}
+      }
+      &:active {
+        border-color: ${palette.outline};
+        ${state.primary.pressed};
+      }
+    `;
+  }
+);
