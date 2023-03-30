4b986a22b5c7c41d7309359814d2d319f83b54ed
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-03-29 20:37:32 -0700

    Import logo to navbar

--- california-dev/src/components/Navigation/Nav.jsx
+++ california-dev/src/components/Navigation/Nav.jsx
@@ -1,23 +1,13 @@
 import * as Styled from "./styles";
-import {HamburgerMenu} from "../common/HamburgerMenu";
+import { Logo } from "assets/images/logo/Logo";
+import { HamburgerMenu } from "../common/HamburgerMenu";
 export function Nav() {
-  //  const [toggle, setToggle] = useState(false);
-  return <Styled.Nav>
-    <HamburgerMenu />
-  </Styled.Nav>;
+  return (
+    <Styled.Nav>
+      <div id="wrapper">
+        <Logo alt="logo image"/>
+        <HamburgerMenu />
+      </div>
+    </Styled.Nav>
+  );
 }
-
-// const Nav = styled.nav((props) => {
-//   console.log(props);
-//   return `
-//     display: flex;
-//     background-color: "black';
-//     width: 100%;
-//   `
-// })
-// const Nav = styled.nav`
-//   height: 56px;
-//   background: ${({ theme: { elevation } }) => elevation.level1.surfaceColor};
-//   box-shadow: ${({ theme: { elevation } }) => elevation.level1.shadow};
-
-// `;
