6d8ff6ecd8a05c0dc5bf676a40ce3496109173d2
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-04-03 01:34:15 -0700

    style(Button): Improve the Button component by adding JSDoc comments for better documentation and code readability.

--- california-dev/src/components/common/Button/Button.jsx
+++ california-dev/src/components/common/Button/Button.jsx
@@ -6,7 +6,7 @@ import {
   OutlinedBtn,
 } from "./styles";
 
-// ENUMS
+// ENUMS representing different button style variants
 const ButtonVariants = {
   FILLED: "filled",
   ELEVATED: "elevated",
@@ -15,6 +15,7 @@ const ButtonVariants = {
   DEFAULT: "default",
 };
 
+// Mapping object to link button style variants with their respective styled components
 const ButtonComponents = {
   [ButtonVariants.FILLED]: FilledBtn,
   [ButtonVariants.ELEVATED]: ElevatedBtn,
@@ -23,13 +24,24 @@ const ButtonComponents = {
   [ButtonVariants.DEFAULT]: DefaultBtn,
 };
 
+/**
+ * Button component which renders a styled button based on the provided variant prop.
+ * @param {string} variant - The button style variant to be applied (default, filled, elevated, tonal, or outlined)
+ * @param {string} text - The text label for the button (alternative to using children)
+ * @param {React.ReactNode} children - The children nodes to be rendered inside the button (alternative to using text prop)
+ * @param {Object} props - Additional props to be passed to the button component
+ * @returns {React.ReactNode} - The styled button component with the specified label and variant
+ */
 export function Button({ variant, text, children, ...props }) {
+  // Use children or text as the button label, with children taking precedence
   const buttonLabel = children || text;
 
+  // Warn if no label is provided for the button
   if (!buttonLabel) {
     console.warn("Please provide a label for this Button component.");
   }
 
+  // Select the appropriate button component based on the variant prop, using DefaultBtn as a fallback
   const ButtonComponent = ButtonComponents[variant] || DefaultBtn;
 
   return <ButtonComponent {...props}>{buttonLabel}</ButtonComponent>;
