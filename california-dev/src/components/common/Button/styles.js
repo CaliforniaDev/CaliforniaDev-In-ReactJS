608cdab494a5d257126251b7aa609ec8f6a90c04
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-04-07 14:19:24 -0700

    style(buttons): Refactor button styles

--- california-dev/src/components/common/Button/styles.js
+++ california-dev/src/components/common/Button/styles.js
@@ -9,7 +9,6 @@ export const DefaultBtn = createDynamicButton(
       justify-content: center;
       align-items: center;
       padding: 1rem 2.4rem;
-      gap: 0.8rem;
       height: 4rem;
       outline-color: transparent;
       border: none;
@@ -41,7 +40,7 @@ export const ElevatedBtn = styled(DefaultBtn)(
         ${elevation.surface.level2};
         ${elevation.shadow.level2};
       }
-      &:focus-visible {
+      &:focus {
         ${state.primary.focus};
         ${elevation.surface.level1};
         ${elevation.shadow.level1};
@@ -67,7 +66,7 @@ export const FilledBtn = styled(DefaultBtn)(
         ${state.onPrimary.hover};
         ${elevation.shadow.level1};
       }
-      &:focus-visible {
+      &:focus {
         ${state.onPrimary.focus};
         ${elevation.shadow.level0};
       }
@@ -89,7 +88,7 @@ export const TonalBtn = styled(DefaultBtn)(
         ${state.onSecondaryContainer.hover};
         ${elevation.shadow.level1};
       }
-      &:focus-visible {
+      &:focus{
         ${state.onSecondaryContainer.focus};
         ${elevation.shadow.level0};
       }
@@ -113,7 +112,7 @@ export const OutlinedBtn = styled(DefaultBtn)(
       &:hover {
         ${state.primary.hover}
       }
-      &:focus-visible {
+      &:focus{
         border-color: ${palette.primary};
         ${state.primary.focus}
       }
