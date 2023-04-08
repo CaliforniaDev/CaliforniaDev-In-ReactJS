6636accff4b2427b79bfc21e249e42badded5682
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-04-08 03:04:59 -0700

    refactor(styles): update StyledBody component styles
    Update the StyledBody component styles to include a top margin of 80px for better spacing and layout. Use the conventional commit format.

--- california-dev/src/components/Body/styles.js
+++ california-dev/src/components/Body/styles.js
@@ -2,6 +2,7 @@ import styled, { css } from "styled-components";
 
 export const StyledBody = styled.main(({ theme: { palette } }) => {
   return css`
+    margin: 80px 24px auto;
     min-height: 100dvh;
     min-height: 100vh;
     background: ${palette.background};
