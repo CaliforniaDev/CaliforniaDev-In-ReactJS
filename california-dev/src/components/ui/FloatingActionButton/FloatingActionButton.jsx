9ec8f602e4591115466df2d74eac94e5fef977e3
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-04-13 03:52:24 -0700

    feat: Add expanded FabMenu and toggleTheme function to FloatingActionButton in React JS
    Add an expanded FabMenu and toggleTheme function to the FloatingActionButton component in React JS.
    The component now includes state to handle the expanded state of the FabMenu and a toggleTheme function
    to update the theme mode of the application. The FabButton component now includes a size prop to control
    the size of the button when expanded and a conditional render to switch between displaying the CogIcon
    or the expanded FabMenu. These changes provide a more dynamic and interactive user experience in the
    React JS application.

--- california-dev/src/components/ui/FloatingActionButton/FloatingActionButton.jsx
+++ california-dev/src/components/ui/FloatingActionButton/FloatingActionButton.jsx
@@ -1,9 +1,22 @@
-import { FabContainer, FabButton } from "./styles";
+import React, { useState } from "react";
+
+import { FabContainer, FabButton, FabMenu } from "./styles";
 import { CogIcon } from "assets/images/icons/CogIcon";
-export const FloatingActionButton = ({ onClick }) => {
+export const FloatingActionButton = ({ toggleTheme }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleClick = () => setExpanded((prevState) => !prevState);
+  const menuSize = "180px";
   return (
     <FabContainer>
-      <FabButton onClick={onClick}>{<CogIcon />}</FabButton>
+      <FabButton size={menuSize} expanded={expanded} onClick={handleClick}>
+        <FabMenu size={menuSize} expanded={expanded}>
+          <ul>
+            <li onClick={toggleTheme}>Theme Mode</li>
+          </ul>
+        </FabMenu>
+        {!expanded && <CogIcon />}
+      </FabButton>
     </FabContainer>
   );
 };
