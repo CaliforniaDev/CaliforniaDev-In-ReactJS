6d31845fa94d0815d16ca430190182f15efd15e3
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-04-13 03:51:48 -0700

    feat: Add FabMenu component and update FabButton component in React JS
    Add a new FabMenu component and update the FabButton component in React JS to support it.
    The FabMenu component includes display logic for a flexible menu that expands upon user interaction.
    The FabButton component now accepts additional props to control the expanded and size states of the component.
    These changes provide a more flexible and user-friendly experience for users interacting with the React JS application.

--- california-dev/src/components/ui/FloatingActionButton/styles.js
+++ california-dev/src/components/ui/FloatingActionButton/styles.js
@@ -7,24 +7,42 @@ export const FabContainer = styled.div`
   z-index: 1000;
 `;
 
+export const FabMenu = styled.div`
+  display: ${({ expanded }) => (expanded ? "flex" : "none")};
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  border-radius: 1.6rem;
+  padding: 8px;
+  width: ${({ expanded, size }) => (expanded ? size : "0")};
+  height: ${({ expanded, size }) => (expanded ? size : "0")};
+  overflow: hidden;
+  transition: width 0.3s, height 0.3s;
+`;
+
 export const FabButton = styled.button(
-  ({ theme: { palette, elevation, state } }) => {
+  ({ theme: { palette, elevation, state }, expanded, size }) => {
     return css`
-      display: inline-flex;
+      display: flex;
       justify-content: center;
       align-items: center;
-      width: 5.6rem;
-      height: 5.6rem;
+      position: relative;
+      z-index: 1;
+      width: ${expanded ? size : "5.6rem"};
+      height: ${expanded ? size : "5.6rem"};
       border-radius: 1.6rem;
       border: none;
       cursor: pointer;
       outline-color: transparent;
-      transition: background-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
-      position: relative;
-      z-index: 1;
+      transition: background-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out,
+        width 0.3s, height 0.3s;
       background: ${palette.secondaryContainer};
+      opacity: 0.8;
       ${elevation.shadow.level3};
-      
+
       #vector {
         fill: ${palette.onSecondaryContainer};
       }
