18dfb88e1cc0698fe1d6df41dac62c05fc13767b
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-05-22 01:30:50 -0700

    chore(StyledSection): add avatar shapes and styling
    - Add avatar shapes styling to create a decorative element with SVG dots
    - Update the styling for the link-items-container and social-links classes
    
    BREAKING CHANGE: No breaking changes introduced

--- california-dev/src/components/Section/Home/styles.js
+++ california-dev/src/components/Section/Home/styles.js
@@ -1,33 +1,50 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
 
-export const StyledSection = styled.section(({ theme: { palette, typography } }) => {
-  return css`
-  display: flex;
-  flex-direction: column;
-  gap: 1.4rem;
-  height: calc(100vh - 80px - 40px);
-  padding: 4rem 2.4rem 0;
+export const StyledSection = styled.section(
+  ({ theme: { palette, typography } }) => {
+    return css`
+      display: flex;
+      flex-direction: column;
+      gap: 1.4rem;
+      height: calc(100vh - 80px - 40px);
+      padding: 4rem 2.4rem 0;
 
-  .display {
-    ${typography.display.medium}
-  }
+      .display {
+        ${typography.display.medium}
+      }
 
-  .headline {
-    ${typography.headline.medium}
-  }
+      .headline {
+        ${typography.headline.medium}
+      }
 
-  .accent-color {
-    color: ${palette.secondary};
-  }
+      .accent-color {
+        color: ${palette.secondary};
+      }
 
-  .link-items-container {
-    display: flex;
-    gap: 1.6rem;
-    align-items: center;
+      #avatar-shapes {
+        top: -25%;
+        width: 40em;
+        height: 126%;
+        right: -5.4em;
+        position: absolute;
+
+        #primary-svg-dots {
+          fill: ${palette.primary};
+        }
+        #secondary-svg-dots {
+          fill: ${palette.secondary};
+        }
+      }
+
+      .link-items-container {
+        display: flex;
+        gap: 1.6rem;
+        align-items: center;
+      }
+      .social-links {
+        display: flex;
+        gap: 1.6rem;
+      }
+    `;
   }
-  .social-links {
-    display: flex;
-    gap: 1.6rem;
-  }
-  `
-});
+);
