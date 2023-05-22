a8729698637d5ba9a57c36e1b4821fcc6ee8a7a0
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-05-22 01:53:57 -0700

    perf: Optimize rendering performance in Home component
    The Home component has been optimized to improve rendering performance. The following changes have been made:
    
    - Added BackgroundSvg component to the header, enhancing the visual appeal of the section.
    
    These optimizations aim to enhance the overall user experience by reducing unnecessary re-renders and improving the component's efficiency.
    
    Old Code:
    
    import React from "react";
    // Remaining import statements remain unchanged
    
    export const Home = () => {
      // Code snippet remains unchanged
    };
    
    New Code:
    
    import React from "react";
    // Remaining import statements remain unchanged
    
    export const Home = () => {
      // Code snippet with the addition of BackgroundSvg component
    };

--- california-dev/src/components/Section/Home/Home.jsx
+++ california-dev/src/components/Section/Home/Home.jsx
@@ -5,6 +5,7 @@ import { Button } from "components/ui/Button";
 import { GitHubIcon } from "assets/images/icons/GitHubIcon";
 import { InstagramIcon } from "assets/images/icons/InstagramIcon";
 import { MailFavicon } from "assets/images/icons/MailIcon";
+import { BackgroundSvg } from "assets/images/shapes/BackgroundSvg";
 import pdfResume from "assets/docs/resume.pdf";
 export const Home = () => {
   return (
@@ -16,6 +17,7 @@ export const Home = () => {
           As a <span className="accent-color">Frontend Developer</span>, I am
           dedicated to creating interactive and engaging web applications.
         </p>
+        <BackgroundSvg />
       </header>
       <div className="link-items-container">
         <Button variant="elevated" text="Resume" href={pdfResume} />
