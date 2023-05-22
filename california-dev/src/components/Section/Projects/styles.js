1d4c361a7933c8dfca42ed794e498f98aa422ccd
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-05-22 01:29:21 -0700

    chore(CardContainer): update styles for project card and add dynamic height transition
    - Adjust min-width and min-height properties to ensure a minimum size for the CardContainer
    - Add max-height property to CardContainer to control the dynamic height transition based on the isOpen prop
    - Set transition duration to 0.5s ease-in-out for the max-height property
    - Update class names and selectors to match the new structure and class names of the project card
    
    BREAKING CHANGE: No breaking changes introduced

--- california-dev/src/components/Section/Projects/styles.js
+++ california-dev/src/components/Section/Projects/styles.js
@@ -1,58 +1,83 @@
 import styled, { css } from "styled-components";
 
-
-export const ProjectSection = styled.section(({ theme: { palette, typography } }) => {
-  return css`
-    display: flex;
-    flex-direction: column;
-    color: ${palette.onBackground};
-    padding: 4rem 2.4rem;
-    gap: 3.2rem;
-
-    h2 {
-      ${typography.headline.small};
-    }
-    h3 {
-      ${typography.title.large};
-    }
-  `;
-});
-
-export const CardContainer = styled.div(({ theme: { palette, elevation } }) => {
-  return css`
-    display: flex;
-    flex-direction: column;
-    width: 312px;
-    height: 272px;
-    border-radius: 12px;
-    color: ${palette.onSurfaceVariant};
-    ${elevation.surface.level1};
-    ${elevation.shadow.level1};
-    margin-bottom: 4rem;
-    overflow: hidden;
-
-    .project-media-container {
+export const ProjectSection = styled.section(
+  ({ theme: { palette, typography } }) => {
+    return css`
       display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: ${palette.surfaceVariant};
-      border-radius: 12px;
-      height: 200px;
+      flex-direction: column;
+      color: ${palette.onBackground};
+      padding: 4rem 2.4rem;
+      gap: 3.2rem;
 
-      img {
-        transform: translateX(5px);
-        width: 216px;
+      h2 {
+        ${typography.headline.small};
       }
-    }
+      h3 {
+        ${typography.title.large};
+      }
+    `;
+  }
+);
 
-    .project-description {
-      padding: 1.6rem 1.6rem 0 1.6rem;
-    }
-    .project-header {
+export const CardContainer = styled.div(
+  ({ theme: { palette, elevation }, isOpen }) => {
+    console.log(isOpen);
+    return css`
       display: flex;
-      justify-content: space-between;
-      align-items: center;
-      width: 100%;
-    }
-  `;
-});
+      flex-direction: column;
+      min-width: 312px;
+      min-height: 272px;
+      border-radius: 12px;
+      margin-bottom: 4rem;
+      max-height: ${isOpen ? "1000px" : "272px"};
+      transition: max-height 0.5s ease-in-out;
+      overflow: hidden;
+      color: ${palette.onSurfaceVariant};
+      ${elevation.surface.level1};
+      ${elevation.shadow.level1};
+
+      .project-card__image-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 12px;
+        min-height: 200px;
+        position: relative;
+        background-color: ${palette.surfaceVariant};
+
+        img {
+          transform: translateX(5px);
+          width: 216px;
+        }
+      }
+
+      .project-card__header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        padding-bottom: 2.4rem;
+      }
+      .project-card__info {
+        padding: 1.6rem;
+        p {
+          padding-bottom: 1.6rem;
+        }
+      }
+
+      .project-card__icon {
+        transform: ${isOpen ? "rotate(180deg)" : "rotate(0deg)"};
+        transition: transform 0.3s ease-in-out;
+        .path {
+          fill: ${palette.secondary};
+        }
+      }
+
+      .project-card__action-buttons {
+        display: flex;
+        justify-content: flex-end;
+        gap: 0.8rem;
+      }
+    `;
+  }
+);
