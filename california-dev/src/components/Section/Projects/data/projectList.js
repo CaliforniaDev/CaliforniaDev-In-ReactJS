b795b031a53eb7127055f200fb47886429d038aa
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-05-22 01:29:49 -0700

    chore(projectList): update project descriptions for MemoryGame and CupKayz
    - Update the project description for the MemoryGame project
    - Update the project descriptions for the CupKayz project
    
    BREAKING CHANGE: No breaking changes introduced

---
+++ california-dev/src/components/Section/Projects/data/projectList.js
@@ -0,0 +1,29 @@
+import MemoryGameImage from "assets/images/projects/MemoryGame.png";
+
+export const projectList = {
+  MemoryGame: {
+    id: "memory-game",
+    title: "Memory Game",
+    description: [`A simple memory game built with React`],
+    imageSrc: MemoryGameImage,
+  },
+
+  CupKayz: {
+    id: "cup-kayz",
+    title: "CupKayz",
+    description: [
+      `As a skilled web developer, I built a 
+      responsive e-commerce web application that can be 
+      used on various devices. To improve delivery service, 
+      I integrated the Google Maps API for map locations, 
+      and added the Autocomplete feature for address input for 
+      enhanced user experience.`,
+
+      `To plan out the design, I created 
+      low and high fidelity wire-frames using Adobe XD's 8pt grid system. 
+      I also used vanilla JavaScript, ensuring the project was completed 
+      without reliance on any libraries. This approach helped me to have 
+      a better control and maintainability of the final solution.`,
+    ],
+  },
+};
