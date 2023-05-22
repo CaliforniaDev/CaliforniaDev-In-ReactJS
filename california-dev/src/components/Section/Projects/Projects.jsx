60d4ecd697a137532e5a8cc0c672d55bcca46a74
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-05-22 01:28:13 -0700

    feat(Projects): improve project card functionality and UI
    - Refactor the code to use useRef and useEffect hooks for scrolling to the expanded project card
    - Update the project card structure and class names for improved styling and readability
    - Add ExpandedCardContent component for rendering project description paragraphs and action buttons
    - Use projectList from data/projectList file to populate project items
    - Modify toggleOpen function to handle opening and closing of individual projects
    
    BREAKING CHANGE: No breaking changes introduced

--- california-dev/src/components/Section/Projects/Projects.jsx
+++ california-dev/src/components/Section/Projects/Projects.jsx
@@ -1,53 +1,93 @@
-import { useState } from "react";
-import { CardContainer } from "./styles";
-import { ProjectSection } from "./styles";
-import MemoryGameImage from "assets/images/projects/MemoryGame.png";
+import { useRef, useEffect, useState } from "react";
+import { CardContainer, ProjectSection } from "./styles";
+import { Button } from "components/ui/Button";
 import { DoubleArrowDown } from "assets/images/icons/DoubleArrowDown";
+import { projectList } from "./data/projectList";
 
-const projectItems = {
-  MemoryGame: {
-    title: "Memory Game",
-    description: "A simple memory game built with React",
-    imageSrc: MemoryGameImage,
-  },
-  //Add more projects here
-};
-const ProjectCard = ({ project, isOpen, toggleOpen }) => {
+const ExpandedCardContent = ({ project, isOpen }) => {
+  if (!isOpen) return null;
+  const paragraphs = project.description;
   return (
-    <CardContainer onClick={toggleOpen}>
-      <figure className="project-media-container">
-        <img src={project.imageSrc} alt={project.title} />
-      </figure>
-      <article className="project-description">
-        <header className="project-header">
-          <h3>{project.title}</h3>
-          <DoubleArrowDown className="icon" onClick={toggleOpen} />
-        </header>
-        {isOpen && <p>{project.description}</p>}
+    <>
+      {paragraphs.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
+      <div className="project-card__action-buttons">
+        <Button text="Repository" variant="outlined" />
+        <Button text="Live" variant="elevated" />
+      </div>
+    </>
+  );
+};
+
+const ProjectCard = ({ project, isOpen, toggleOpen }) => {
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen && cardRef.current) {
+      window.scrollTo({
+        top: cardRef.current.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  }, [isOpen]);
+
+  return (
+    <CardContainer
+      onClick={toggleOpen}
+      className="project-card"
+      isOpen={isOpen}
+      ref={cardRef}
+    >
+      <article className="project-card__content-wrapper">
+        <figure className="project-card__image-container">
+          <img src={project.imageSrc} alt={project.title} />
+        </figure>
+        <div className="project-card__info">
+          <header className="project-card__header">
+            <h2>{project.title}</h2>
+            <DoubleArrowDown className="project-card__icon" />
+          </header>
+          <ExpandedCardContent project={project} isOpen={isOpen} />
+        </div>
       </article>
     </CardContainer>
   );
 };
 
-export const Projects = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleOpen = () => {
-    setIsOpen(!isOpen);
-  };
+
+export const Projects = () => {
+  const [openProjects, setOpenProjects] = useState({});
+
+  function toggleOpen(projectId) {
+    return function () {
+      setOpenProjects((prevState) => ({
+        ...Object.keys(prevState).reduce(
+          (acc, id) => ({ ...acc, [id]: false }),
+          {}
+        ),
+        [projectId]: !prevState[projectId],
+      }));
+    };
+  }
+
+  function renderProjects() {
+    return Object.values(projectList).map((project) => (
+      <div key={project.id} className="items-wrapper">
+        <ProjectCard
+          project={project}
+          isOpen={openProjects[project.id]}
+          toggleOpen={toggleOpen(project.id)}
+        />
+      </div>
+    ));
+  }
+
   return (
     <ProjectSection aria-label="Recent Projects">
       <h2>Recent Projects</h2>
-      {Object.values(projectItems).map((project, index) => (
-        <div className="items-wrapper">
-          <ProjectCard
-            key={index}
-            project={project}
-            isOpen={isOpen}
-            toggleOpen={toggleOpen}
-          />
-        </div>
-      ))}
+      {renderProjects()}
     </ProjectSection>
   );
 };
