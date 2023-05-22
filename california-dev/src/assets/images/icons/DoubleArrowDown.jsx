fb7b9ed5308b150d80d1e997f304e8d1edc141d3
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-05-22 01:53:00 -0700

    feat: Add className prop to DoubleArrowDown component
    The DoubleArrowDown component now accepts a className prop, allowing the customization of its styling. This change enables users to apply specific CSS classes to the component and modify its appearance as needed.
    
    Old Code:
    
    export const DoubleArrowDown = () => {
      // Code snippet remains unchanged
    };
    
    New Code:
    
    export const DoubleArrowDown = ({className}) => {
      // Code snippet with the addition of className prop
    };

--- california-dev/src/assets/images/icons/DoubleArrowDown.jsx
+++ california-dev/src/assets/images/icons/DoubleArrowDown.jsx
@@ -1,6 +1,6 @@
-export const DoubleArrowDown = () => {
+export const DoubleArrowDown = ({className}) => {
   return (
-    <figure>
+    <figure className={className}>
       <svg
         width="24"
         height="24"
@@ -10,13 +10,13 @@ export const DoubleArrowDown = () => {
       >
         <g id="double-arrow-icon">
             <path
-            className="vector"
+            className="path"
               id="first-arrow"
               d="M17.1 12.3C16.7 11.9 16.1 11.9 15.7 12.3L12.2 15.8L8.7 12.3C8.3 11.9 7.7 11.9 7.3 12.3C6.9 12.7 6.9 13.3 7.3 13.7L11.5 17.9C11.7 18.1 11.9 18.2 12.2 18.2C12.5 18.2 12.7 18.1 12.9 17.9L17.1 13.7C17.5 13.3 17.5 12.7 17.1 12.3Z"
               fill="#FFB0CD"
             />
             <path
-            className="vector"
+            className="path"
               id="second-arrow"
               d="M17.1 5.3C16.7 4.9 16.1 4.9 15.7 5.3L12.2 8.8L8.7 5.3C8.3 4.9 7.7 4.9 7.3 5.3C6.9 5.7 6.9 6.3 7.3 6.7L11.5 10.9C11.7 11.1 11.9 11.2 12.2 11.2C12.5 11.2 12.7 11.1 12.9 10.9L17.1 6.7C17.5 6.3 17.5 5.7 17.1 5.3Z"
               fill="#FFB0CD"
