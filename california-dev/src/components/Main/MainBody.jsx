14e955fa936a9ae884733bb17c655c5500c9a8a8
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-05-25 06:58:34 -0700

    Remove: unused assets, prepare for replacment

--- california-dev/src/components/Main/MainBody.jsx
+++ california-dev/src/components/Main/MainBody.jsx
@@ -2,6 +2,7 @@ import { MainContainer } from "./styles";
 import { Home } from "../Section/Home";
 import { Skills } from "../Section/Skills";
 import { Projects } from "../Section/Projects";
+import { Technical } from "../Section/Technical";
 import { Nav } from "components/Navigation/Nav";
 export function MainBody() {
   return (
@@ -10,6 +11,7 @@ export function MainBody() {
       <Home />
       <Skills />
       <Projects />
+      <Technical />
     </MainContainer>
   );
 }
