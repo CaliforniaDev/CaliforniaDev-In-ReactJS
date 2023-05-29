c67c39280bffb4e3f8e6cb5629b5a55d2b037b23
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-05-29 03:01:55 -0700

    refactor: Remove duplicate entry in projectList
    The duplicate entry for the "CupKayz" project in the projectList has been removed to avoid redundancy and maintain consistency. The projectList now contains the following projects:
    
    - MemoryGame: A simple memory game built with React. It has the id "memory-game" and uses the MemoryGameImage as the image source.
    - CupKayz: A responsive e-commerce web application with various features such as integration with the Google Maps API for map locations and Autocomplete for address input. It has the id "cup-kayz" and uses the CupKayzImage as the image source. The description includes information about wire-framing using Adobe XD's 8pt grid system and the use of vanilla JavaScript for development.

--- california-dev/src/components/Section/Projects/data/projectList.js
+++ california-dev/src/components/Section/Projects/data/projectList.js
@@ -30,24 +30,4 @@ export const projectList = {
     ],
     
   },
-    CupKayz: {
-    id: "cup-kayz",
-    title: "CupKayz",
-    imageSrc: CupKayzImage,
-    description: [
-      `As a skilled web developer, I built a 
-      responsive e-commerce web application that can be 
-      used on various devices. To improve delivery service, 
-      I integrated the Google Maps API for map locations, 
-      and added the Autocomplete feature for address input for 
-      enhanced user experience.`,
-
-      `To plan out the design, I created 
-      low and high fidelity wire-frames using Adobe XD's 8pt grid system. 
-      I also used vanilla JavaScript, ensuring the project was completed 
-      without reliance on any libraries. This approach helped me to have 
-      a better control and maintainability of the final solution.`,
-    ],
-    
-  },
 };
