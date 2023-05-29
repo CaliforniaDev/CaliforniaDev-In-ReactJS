ac98d95779e9c908cac815853504f85d0df27a14
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-05-28 23:15:07 -0700

    feat: Add new project entry to projectList
    A new project entry has been added to the projectList object.

--- california-dev/src/components/Section/Projects/data/projectList.js
+++ california-dev/src/components/Section/Projects/data/projectList.js
@@ -1,16 +1,19 @@
 import MemoryGameImage from "assets/images/projects/MemoryGame.png";
+import CupKayzImage from "assets/images/projects/CupKayz.png";
 
 export const projectList = {
   MemoryGame: {
     id: "memory-game",
     title: "Memory Game",
-    description: [`A simple memory game built with React`],
     imageSrc: MemoryGameImage,
+    description: [`A simple memory game built with React`],
+    
   },
 
   CupKayz: {
     id: "cup-kayz",
     title: "CupKayz",
+    imageSrc: CupKayzImage,
     description: [
       `As a skilled web developer, I built a 
       responsive e-commerce web application that can be 
@@ -25,5 +28,26 @@ export const projectList = {
       without reliance on any libraries. This approach helped me to have 
       a better control and maintainability of the final solution.`,
     ],
+    
+  },
+    CupKayz: {
+    id: "cup-kayz",
+    title: "CupKayz",
+    imageSrc: CupKayzImage,
+    description: [
+      `As a skilled web developer, I built a 
+      responsive e-commerce web application that can be 
+      used on various devices. To improve delivery service, 
+      I integrated the Google Maps API for map locations, 
+      and added the Autocomplete feature for address input for 
+      enhanced user experience.`,
+
+      `To plan out the design, I created 
+      low and high fidelity wire-frames using Adobe XD's 8pt grid system. 
+      I also used vanilla JavaScript, ensuring the project was completed 
+      without reliance on any libraries. This approach helped me to have 
+      a better control and maintainability of the final solution.`,
+    ],
+    
   },
 };
