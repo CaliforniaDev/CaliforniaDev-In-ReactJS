f73011a3ba7b526f76ec5e2039891d3937ce6798
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-06-06 20:16:54 -0700

    feat: add new TextField component
    Introduces a new TextField component along with associated styling.
    The component includes three exported styled-components: InputField,
    Input, and Label. The TextFieldStyles file includes CSS-in-JS styling
    definitions for these components.
    
    The InputField is a block-level, relative-positioned div with full width,
    with styling for hover states. The Input styled component defines an input
    field with various theme-based properties, handling both focused and
    multiline states. Lastly, the Label styled component handles focused and
    populated states.
    
    These components use the theme props for dynamic styling, allowing the
    components to adapt to various theme changes.
    
    This addition will significantly enhance the UI library, making form
    implementation more flexible and consistent across the application.

---
+++ california-dev/src/components/ui/TextField/TextField.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Input, InputField, Label } from "./TextFieldStyles";
+
+export const TextField = ({ label, type, multiline, ...props }) => {
+  const [text, setText] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  function handleChange(event) {
+    setText(event.target.value);
+    console.log(text);
+  }
+
+  function handleFocus() {
+    setIsFocused(true);
+  }
+
+  function handleBlur() {
+    setIsFocused(false);
+  }
+
+  const InputOrTextarea = multiline ? "textarea" : "input";
+
+  return (
+    <InputField>
+      <Input
+        as={InputOrTextarea}
+        type={type || "text"}
+        value={text}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        isFocused={isFocused}
+        multiline={multiline}
+        {...props}
+      />
+      <Label isFocused={isFocused} isPopulated={text !== ""}>
+        {label || "Your Label"}
+      </Label>
+    </InputField>
+  );
+};
