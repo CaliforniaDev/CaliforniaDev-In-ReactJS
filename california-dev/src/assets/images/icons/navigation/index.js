1bce26d0b0691cfe9b2c4fd8da3917adaab4e6d5
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-06-19 04:55:15 -0700

    feat: Add LightBulbIcon component and use it in JSX
    This commit adds a new LightBulbIcon component to the project, which renders a light bulb icon using SVG. The component is added to the project's icon library and can be used throughout the application.
    
    In addition to adding the LightBulbIcon component, this commit updates several components to use the new icon in JSX. For example, the `Header` component now uses the LightBulbIcon component to render a light bulb icon in the header.
    
    Overall, this commit adds a new icon to the project and improves the consistency and usability of the application's icon library.

---
+++ california-dev/src/assets/images/icons/navigation/index.js
@@ -0,0 +1,7 @@
+export { CogIcon } from "./CogIcon";
+export { HomeIcon } from "./HomeIcon";
+export { LightBulbIcon } from "./LightBulbIcon";
+export { MonitorIcon } from "./MonitorIcon";
+export { EmailIcon } from "./EmailIcon";
+export {ImagesIcon} from "./ImagesIcon";
+export {ChartIcon} from "./ChartIcon";
