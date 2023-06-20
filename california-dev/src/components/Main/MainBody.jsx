972675d2833c8a323b794dcb26c51e0323b2c4a6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-06-19 19:33:03 -0700

    feat: Add NavRail component to MainBody.jsx

--- california-dev/src/components/Main/MainBody.jsx
+++ california-dev/src/components/Main/MainBody.jsx
@@ -5,10 +5,12 @@ import { Projects } from "../sections/Projects";
 import { Technical } from "../sections/Technical";
 import { Contact } from "../sections/Contact";
 import { Nav } from "components/Navigation/Nav";
+import {NavRail} from "components/ui/Navigation/Desktop/NavRail";
 export function MainBody() {
   return (
     <MainContainer>
       <Nav />
+      <NavRail />
       <Home />
       <Skills />
       <Projects />
