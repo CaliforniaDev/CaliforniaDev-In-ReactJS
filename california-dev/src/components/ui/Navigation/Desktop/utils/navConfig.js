171b3c75faf7e94ec4f62927f85f130f9b8e4788
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-06-19 20:57:05 -0700

    feat(navConfig): add navigation configuration file
    - Add new navConfig.js file to manage navigation items
    - This improves modularity and makes it easier to manage nav items

---
+++ california-dev/src/components/ui/Navigation/Desktop/utils/navConfig.js
@@ -0,0 +1,15 @@
+import {
+  HomeIcon,
+  ChartIcon,
+  ImagesIcon,
+  LightBulbIcon,
+  EmailIcon,
+} from "assets/images/icons/navigation";
+
+export const navItems = [
+  { Icon: HomeIcon, name: "Home" },
+  { Icon: ChartIcon, name: "Workflow" },
+  { Icon: ImagesIcon, name: "Projects" },
+  { Icon: LightBulbIcon, name: "Skills" },
+  { Icon: EmailIcon, name: "Contact Us" },
+];
