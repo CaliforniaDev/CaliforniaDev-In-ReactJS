4dfd3ae8b4c8673901078e086c2ae7db43e2751a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-06-19 19:29:33 -0700

    feat: Add media queries to light and dark themes

--- california-dev/src/themes/dark/darkTheme.js
+++ california-dev/src/themes/dark/darkTheme.js
@@ -2,10 +2,12 @@ import { palette } from "./palette";
 import { elevation } from "./elevation";
 import { typography } from "./typography";
 import { state } from "./state";
+import { media } from "../utils/mediaQueries";
 
 export const darkTheme = {
   palette,
   elevation,
   typography,
   state,
+  media,
 };
--- california-dev/src/themes/light/lightTheme.js
+++ california-dev/src/themes/light/lightTheme.js
@@ -2,10 +2,12 @@ import { palette } from "./palette";
 import { elevation } from "./elevation";
 import { typography } from "./typography";
 import { state } from "./state";
+import { media } from "../utils/mediaQueries";
 
 export const lightTheme = {
   palette,
   elevation,
   typography,
   state,
+  media,
 };
