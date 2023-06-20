1911d7dcfca6d58bf9ad288d69103a4253c51c4b
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-06-19 19:27:20 -0700

    feat: Add mediaQueries.js file for managing media queries and padding sizes
    This commit adds a new `mediaQueries.js` file to the project, which provides a centralized location for defining media queries and padding sizes that can be used throughout the application. The file defines a `media` object that contains media query functions for each breakpoint defined in the `BREAKPOINTS` object, as well as a `padding` property that contains padding sizes defined in the `PADDING_SIZES` object.
    
    The `media` object is created using the `reduce()` method to iterate over the keys in the `BREAKPOINTS` object and create a property for each breakpoint in the `media` object. The `padding` property is created using a similar approach, using the `reduce()` method to iterate over the keys in the `PADDING_SIZES` object and create a property for each padding size in the `media.padding` object.

---
+++ california-dev/src/themes/utils/mediaQueries.js
@@ -0,0 +1,49 @@
+
+
+import { css } from "styled-components";
+
+// This code creates a media object that can be used to create media queries
+// for different screen sizes. It also creates a padding property that can be
+// used to set padding values in different components.
+
+
+const BREAKPOINTS = {
+  LARGE: 1200,
+  DESKTOP: 992,
+  TABLET: 768,
+  PHONE: 576,
+};
+
+const PADDING_SIZES = {
+  SMALL: "24px",
+  MEDIUM: "104px",
+  LARGE: "208px",
+};
+
+// 3. Create a media object
+//    a. Use Object.keys() to get an array of breakpoint keys
+//    b. Use reduce() to iterate over the keys and create a property for each
+//       breakpoint in the media object
+export const media = Object.keys(BREAKPOINTS).reduce((acc, label) => {
+  // 4. Calculate the emSize of the breakpoint
+  const emSize = BREAKPOINTS[label] / 16;
+  // 5. Create a function to return a media query
+  acc[label] = (...args) => {
+    // 6. If the function is called with arguments
+    if (args.length > 0) {
+      // 7. Return a media query
+      return css`
+        @media (min-width: ${emSize}em) {
+          ${css(...args)};
+        }
+      `;
+    }
+  };
+  return acc;
+}, {});
+
+// 8. Add padding property to media object
+media.padding = Object.keys(PADDING_SIZES).reduce((acc, label) => {
+  acc[label] = PADDING_SIZES[label];
+  return acc;
+}, {});
