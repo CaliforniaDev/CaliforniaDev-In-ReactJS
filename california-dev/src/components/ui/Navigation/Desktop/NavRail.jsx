7cb0008f9461d3315bf9edc68bd5a86476641226
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-06-26 03:03:29 -0700

    refactor: update NavRail component
    This commit refactors the `NavRail` component with the following changes:
    
    - Renamed `NavList` to `NavLinksWrapper` for improved semantic naming.
    - Renamed `NavItem` to `NavLink` to accurately represent its purpose.
    - Added a new `Logo` component to display the logo in the `Nav` section.
    - Updated the data import to use `navItemsData` instead of `navItems` for consistency.
    - Added a `useRef` hook to store references to the navigation links.
    - Refactored the event handler functions `handleMouseDown`, `handleMouseUp`, and `handleNavLinkClick` to use the `useCallback` hook for better performance.
    - Implemented smooth scrolling to the clicked navigation link using `scrollIntoView` and the stored references.
    
    These changes improve the readability, maintainability, and functionality of the `NavRail` component.

--- california-dev/src/components/ui/Navigation/Desktop/NavRail.jsx
+++ california-dev/src/components/ui/Navigation/Desktop/NavRail.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
-import { Nav, NavList, NavItem } from "./NavRail.styles";
-import { navItems } from "./utils/navConfig";
+import { useState, useEffect, useRef, useCallback } from "react";
+import { Nav, NavLinksWrapper, NavLink } from "./NavRail.styles";
+import { navItemsData } from "./utils/navConfig";
+import { Logo } from "assets/images/logo";
 
 export const NavRail = () => {
   const [activeAnchor, setActiveAnchor] = useState(null);
   const [pressedAnchor, setPressedAnchor] = useState(null);
+  const navLinksRefs = useRef([]);
 
   useEffect(() => {
     const handleGlobalMouseUp = () => {
@@ -17,32 +19,39 @@ export const NavRail = () => {
     };
   }, []);
 
-  function handleMouseDown(name) {
-    setPressedAnchor(name);
-  }
+  const handleMouseDown = useCallback((id) => {
+    setPressedAnchor(id);
+  }, []);
 
-  function handleMouseUp() {
+  const handleMouseUp = useCallback(() => {
     setPressedAnchor(null);
-  }
+  }, []);
+
+  const handleNavLinkClick = useCallback((id, index) => {
+    setActiveAnchor(id);
+    navLinksRefs.current[index].scrollIntoView({ behavior: "smooth" });
+  }, [navLinksRefs]);
 
   return (
     <Nav>
-      <NavList>
-        {navItems.map(({ Icon, name }) => (
-          <NavItem
-            tabIndex="0"
-            isActive={activeAnchor === name}
-            isPressed={pressedAnchor === name}
-            onMouseDown={() => handleMouseDown(name)}
+      <Logo className="logo" />
+      <NavLinksWrapper>
+        {navItemsData.map(({ Icon, name, id }, index) => (
+          <NavLink
+            ref={(el) => (navLinksRefs.current[index] = el)}
+            key={id}
+            href={`#${id}`}
+            isActive={activeAnchor === id}
+            isPressed={pressedAnchor === id}
+            onMouseDown={() => handleMouseDown(id)}
             onMouseUp={handleMouseUp}
-            onClick={() => setActiveAnchor(name)}
-            key={name}
+            onClick={() => handleNavLinkClick(id, index)}
           >
-            <Icon className="nav-icon" aria-hidden="true" />
+            <Icon className="nav-icon" aria-hidden="true" alt={name} />
             <span className="visually-hidden">{name}</span>
-          </NavItem>
+          </NavLink>
         ))}
-      </NavList>
+      </NavLinksWrapper>
     </Nav>
   );
 };
