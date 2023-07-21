438b823478ba2c9828f87aa00fb481d57fb21e12
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-07-21 03:49:33 -0700

    feat: Update Home.styles.js with enhanced styling and layout
    This commit brings extensive updates to the Home.styles.js file. The purpose of the updates is to align with changes made to the Home component and to improve the overall design and responsiveness of the layout.

--- california-dev/src/components/sections/Home/Home.styles.js
+++ california-dev/src/components/sections/Home/Home.styles.js
@@ -1,50 +1,115 @@
 import styled, { css } from "styled-components";
 
 export const StyledSection = styled.section(
-  ({ theme: { palette, typography } }) => {
+  ({ theme: { palette, elevation, typography, media, profileImg } }) => {
+    console.log(media.padding.SMALL);
     return css`
       display: flex;
       flex-direction: column;
-      gap: 1.4rem;
-      height: calc(100vh - 80px - 40px);
-      padding: 4rem 2.4rem 0;
+      min-height: calc(100vh - 80px - 40px);
+      padding: 4rem ${media.padding.SMALL};
 
       .display {
         ${typography.display.medium}
       }
-
       .headline {
         ${typography.headline.medium}
       }
-
+      .primary-color {
+        color: ${palette.primary};
+      }
       .accent-color {
         color: ${palette.secondary};
       }
 
-      #avatar-shapes {
-        top: -25%;
-        width: 40em;
-        height: 126%;
-        right: -5.4em;
-        position: absolute;
-
-        #primary-svg-dots {
-          fill: ${palette.primary};
-        }
-        #secondary-svg-dots {
-          fill: ${palette.secondary};
-        }
-      }
-
       .link-items-container {
         display: flex;
-        gap: 1.6rem;
         align-items: center;
+        gap: 1.6rem;
       }
       .social-links {
         display: flex;
         gap: 1.6rem;
       }
+
+      ${media.DESKTOP`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+
+        padding: ${media.padding.LARGE};
+
+        article {
+          display: flex;
+          flex-direction: column;
+          gap: 1.6rem;
+        }
+        header {
+          width: 46rem;
+        }
+
+        #photo-wrapper {
+          display: flex;
+          position relative;
+          width: 36.8rem;
+          height: 44.8rem;
+        }
+        .profile-img {
+            display: block;
+            width: inherit;
+            height: inherit;
+
+            background-color: ${palette.primary};
+            background-size: cover;
+            background-position-x: center;
+            background-image: url(${profileImg});
+            ${elevation.shadow.level5};
+
+            z-index: 1;
+          }
+          .outline-frame {
+            position: absolute;
+            width: inherit;
+            height: inherit;
+            border: 2px solid ${palette.primary};
+
+            left: -20px;
+            top: -20px;
+          }
+          .filled-frame {
+            position: absolute;
+            width: inherit;
+            height: inherit;
+
+            background-color: ${palette.secondary};
+            right: -20px;
+            bottom: -20px;
+          }
+
+        .corner-svg {
+            position: absolute;
+            width: 108px;
+            height: 108px;
+          }
+
+          .top-left {
+            top: -64px;
+            left: -64px;
+          }
+
+          .bottom-right {
+            bottom: -64px;
+            right: -64px;
+          }
+
+           #topRightPattern circle {
+            fill: ${palette.secondary};
+          }
+          #bottomRightPattern circle {
+            fill: ${palette.primary};
+          }
+      `}
     `;
   }
 );
