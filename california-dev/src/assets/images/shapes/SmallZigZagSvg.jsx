b8b74b2bc1328fd8984a2b618d1909b9ec1c386a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-07-28 17:54:43 -0700

    feat: Add SmallZigZagSvg component

---
+++ california-dev/src/assets/images/shapes/SmallZigZagSvg.jsx
@@ -0,0 +1,10 @@
+export const SmallZigZagSvg = ({className}) => (
+  <svg className={className} width="68" height="68" xmlns="http://www.w3.org/2000/svg"  >
+    <path
+      fill="none"
+      stroke="red"
+      strokeWidth="1.5"
+      d="M1 0v16h16v16h16v16h16"
+    />
+  </svg>
+);
