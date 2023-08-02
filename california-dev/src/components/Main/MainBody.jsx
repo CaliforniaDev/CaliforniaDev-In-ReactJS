ba9fd2cae73c132c3662a5050a5894ce3eff356b
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-08-02 04:56:21 -0700

    style: Reorder imports

--- california-dev/src/components/Main/MainBody.jsx
+++ california-dev/src/components/Main/MainBody.jsx
@@ -1,13 +1,15 @@
+import { useMediaQuery } from "react-responsive";
+import { useTheme } from "context/ThemeContext";
+
 import { MainContainer } from "./styles";
+import { Nav } from "components/Navigation/Nav";
+import { NavRail } from "components/ui/Navigation/Desktop/NavRail";
 import { Home } from "../sections/Home";
 import { Workflow } from "../sections/WorkFlow";
 import { Projects } from "../sections/Projects";
 import { Skills } from "../sections/Skills";
 import { Contact } from "../sections/Contact";
-import { Nav } from "components/Navigation/Nav";
-import { NavRail } from "components/ui/Navigation/Desktop/NavRail";
-import { useMediaQuery } from "react-responsive";
-import { useTheme } from "context/ThemeContext";
+
 
 export function MainBody() {
 
