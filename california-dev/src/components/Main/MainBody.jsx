55eef06bce5a99200dea346d2bc8a8816f7146bc
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-08-04 07:07:23 -0700

    refactor: remove unused theme context from MainBody component

--- california-dev/src/components/Main/MainBody.jsx
+++ california-dev/src/components/Main/MainBody.jsx
@@ -7,18 +7,18 @@ import { Contact } from "../sections/Contact";
 import { Nav } from "components/Navigation/Nav";
 import { NavRail } from "components/ui/Navigation/Desktop/NavRail";
 import { useMediaQuery } from "react-responsive";
-import { useTheme } from "context/ThemeContext";
+
 
 export function MainBody() {
 
-  const { toggleTheme } = useTheme();
+
   const isTabletOrLarger = useMediaQuery({
     query: `(min-width: ${768 / 16}em)`,
   });
 
   return (
     <MainContainer>
-      {isTabletOrLarger ? <NavRail toggleTheme={toggleTheme} /> : <Nav />}
+      {isTabletOrLarger ? <NavRail /> : <Nav />}
       <Home />
       <Workflow />
       <Projects />
