d172172e57ea68b0598c98c6fbaa5c389f4d728b
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-08-14 01:49:39 -0700

    feat(Contact styles): Refactor and enhance styles for Contact section
    - Implement responsive design for different screen sizes, including desktop-specific styles.
    - Rename RadioButtonGroup to RadioButtonFieldset to align with component structure.
    - Add styling for new decorative elements and SVG patterns.
    - Introduce grid layout for field sections and make responsive adjustments.
    - Modify text alignments, typography, and spacing for improved presentation.
    - Enhance fieldset styling by removing default styles and adding custom control.
    
    These updates provide a more adaptive and visually appealing Contact section across various devices and screen sizes.

--- california-dev/src/components/sections/Contact/Contact.styles.js
+++ california-dev/src/components/sections/Contact/Contact.styles.js
@@ -1,48 +1,101 @@
 import styled, { css } from "styled-components";
 
 export const ContactSection = styled.section(
-  ({ theme: { palette, typography, elevation } }) => {
-    return css`
+  ({ theme: { palette, typography, media } }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: left;
+    padding: 40px ${media.padding.SMALL};
+
+    fieldset {
+      border: none; /* removes default border */
+      padding: 0; /* removes default padding */
+      margin: 0; /* removes default margin */
+    }
+    h2 {
+      ${typography.display.small};
+      margin-bottom: 4px;
+    }
+    .contact-subtext,
+    legend {
+      ${typography.title.medium};
+
+      span {
+        white-space: nowrap;
+      }
+    }
+
+    .contact-form {
       display: flex;
       flex-direction: column;
-      align-items: center;
-      text-align: center;
-      padding: 0 2.4rem;
+      width: 100%;
+      gap: 24px;
+      height: auto;
+    }
+    .field-grid {
+      display: grid;
+      grid-template-columns: 1fr;
+      gap: 16px;
+    }
+
+    .submit-button {
+      border-radius: 4px;
+    }
+
+    .decoration-container {
+      display: none;
+    }
+
+    ${media.DESKTOP`
+      padding: 64px ${media.padding.LARGE};
+      flex-direction: row;
+      gap: 16px;
 
       h2 {
-        ${typography.display.small};
+        ${typography.display.large};
       }
-      h3 {
-        ${typography.title.small};
-
-        span {
-          white-space: nowrap;
-        }
+      .contact-subtext {
+        ${typography.headline.small};
       }
-
-      .contact-form {
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        gap: 1.6rem;
-        margin: 1.6rem;
-        height: auto;
+      .field-grid {
+        grid-template-columns: 1fr 1fr;
       }
 
-      .submit-button {
-        border-radius: 4px;
+      .decoration-container {
+        display: block;
+        position: relative;
+        align-self: stretch;
+        width: 200px;
+        background-color: ${palette.primary};
+        border-top-left-radius: 12px;
+        border-bottom-left-radius: 12px;
+        .dots-svg circle { fill: ${palette.onPrimary}; }
+        .eclipse-svg circle { stroke: ${palette.onPrimary}; }
+        .zigzag-svg path { stroke: ${palette.onPrimary}; }
       }
-    `;
-  }
+    `} // End of DESKTOP media query
+  `// End of styles
 );
 
-export const RadioButtonGroup = styled.div(
-  ({ theme: { typography } }) => css`
+export const RadioButtonFieldset = styled.fieldset(
+  ({ theme: { typography, media } }) => css`
     display: flex;
     flex-direction: column;
     text-align: left;
-    gap: 1.6rem;
+    gap: 30px;
     flex-wrap: wrap;
     ${typography.body.medium};
+
+    legend {
+      display: block;
+      width: 100%;
+      margin-bottom: 24px;
+      ${typography.title.medium};
+    }
+
+    ${media.DESKTOP`
+      flex-direction: row;
+    `}
   `
 );
