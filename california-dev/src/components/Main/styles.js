9c5d81d8073ba50bb05f83f235c847760b34b0b6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-08-14 01:54:58 -0700

    fix: Add a value of hidden to the overflow

--- california-dev/src/components/Main/styles.js
+++ california-dev/src/components/Main/styles.js
@@ -2,6 +2,7 @@ import styled, { css } from "styled-components";
 
 export const MainContainer = styled.main(({ theme: { palette, media } }) => {
   return css`
+    overflow: hidden;
     ${media.TABLET`
       margin-left: var(--nav-rail-width);
     `}
