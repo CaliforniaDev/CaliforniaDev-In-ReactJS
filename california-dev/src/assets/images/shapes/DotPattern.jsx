3344822f68b0889409e8896e2263cb29de7fc77a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-08-14 01:36:24 -0700

    refactor: Simplify SVG structure for better maintainability

--- california-dev/src/assets/images/shapes/DotPattern.jsx
+++ california-dev/src/assets/images/shapes/DotPattern.jsx
@@ -1,30 +1,41 @@
-export const DotPattern = ({ className, id }) => {
-
-  if (!id) {
-    console.error("No className or id provided for DotPattern");
-    id = "pattern";
-  }
-
+export const DotPattern = ({ className }) => {
   return (
     <svg
       className={className}
+      width="84"
+      height="84"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      width="108"
-      height="108"
     >
-      <defs>
-        <pattern
-          id={id}
-          width="18"
-          height="18"
-          x="0"
-          y="0"
-          patternUnits="userSpaceOnUse"
-        >
-          <circle cx="9" cy="9" r="2" fill="red" />
-        </pattern>
-      </defs>
-      <path fill={`url(#${id})` } d="M0 0h108v108H0z" />
+      <circle cx="2" cy="2" r="2" fill="red" />
+      <circle cx="22" cy="2" r="2" fill="red" />
+      <circle cx="42" cy="2" r="2" fill="red" />
+      <circle cx="62" cy="2" r="2" fill="red" />
+      <circle cx="82" cy="2" r="2" fill="red" />
+
+      <circle cx="2" cy="22" r="2" fill="red" />
+      <circle cx="22" cy="22" r="2" fill="red" />
+      <circle cx="42" cy="22" r="2" fill="red" />
+      <circle cx="62" cy="22" r="2" fill="red" />
+      <circle cx="82" cy="22" r="2" fill="red" />
+
+      <circle cx="2" cy="42" r="2" fill="red" />
+      <circle cx="22" cy="42" r="2" fill="red" />
+      <circle cx="42" cy="42" r="2" fill="red" />
+      <circle cx="62" cy="42" r="2" fill="red" />
+      <circle cx="82" cy="42" r="2" fill="red" />
+
+      <circle cx="2" cy="62" r="2" fill="red" />
+      <circle cx="22" cy="62" r="2" fill="red" />
+      <circle cx="42" cy="62" r="2" fill="red" />
+      <circle cx="62" cy="62" r="2" fill="red" />
+      <circle cx="82" cy="62" r="2" fill="red" />
+
+      <circle cx="2" cy="82" r="2" fill="red" />
+      <circle cx="22" cy="82" r="2" fill="red" />
+      <circle cx="42" cy="82" r="2" fill="red" />
+      <circle cx="62" cy="82" r="2" fill="red" />
+      <circle cx="82" cy="82" r="2" fill="red" />
     </svg>
   );
 };
