ea1757b9a21ee858ae7ed36cee5d2e547e344001
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-08-17 04:56:15 -0700

    chore: Configure ESLint for the project
    Added ESLint configuration to enhance code quality and maintain
    standard coding practices. This includes setting up appropriate
    rules and plugins for React development.

---
+++ california-dev/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
