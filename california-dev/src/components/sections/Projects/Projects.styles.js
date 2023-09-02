7d5125f87b1904bdaed64afc212a82757afa8324
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-09-02 06:22:31 -0700

    Refactor: Optimized and Simplified ProjectSection Component
    This commit addresses several aspects of the ProjectSection component to make it more maintainable, readable, and optimized for performance. Major changes include:
    
    1. Replaced 'useTheme' import statement with proper formatting: changed from "context/ThemeContext" to 'context/ThemeContext'.
    2. Removed `elevation` related styling and attributes that are not used in the new version.
    3. Replaced the flexbox layout of `.card-container` with a CSS grid layout. This allows for easier management and cleaner code.
    4. Removed redundant media queries that targeted tabletSmall and TABLET, consolidating into a more streamlined approach with minimum width in `@media` rules.
    5. Standardized the usage of media queries to use 'min-width' instead of custom labels like 'media.tabletSmall' or 'media.TABLET'.
    6. Introduced `max-width: var(--desktop-max-width);` for better layout control.
    
    The new implementation is not only cleaner but also more aligned with modern best practices in CSS and React Styled-Components.

--- california-dev/src/components/sections/Projects/Projects.styles.js
+++ california-dev/src/components/sections/Projects/Projects.styles.js
@@ -1,16 +1,18 @@
 import styled, { css } from 'styled-components';
-import {useTheme} from "context/ThemeContext";
+import { useTheme } from 'context/ThemeContext';
 
 export const ProjectSection = styled.section(
-  ({ theme: { palette, elevation, typography, media }, themeMode }) => {
+  ({ theme: { palette, typography, media }, themeMode }) => {
     return css`
       --vertical-padding: 4rem;
       display: flex;
       flex-direction: column;
+      margin-inline: auto;
       color: ${palette.onBackground};
       padding: var(--vertical-padding) ${media.padding.SMALL};
       gap: 1.6rem;
-      
+      max-width: var(--desktop-max-width);
+
       h2 {
         ${typography.headline.small};
       }
@@ -19,85 +21,36 @@ export const ProjectSection = styled.section(
       }
 
       .card-container {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-      }
-      .card {
-        flex-basis: 100%;
-        border-radius: 12px;
-        overflow: hidden;
-        margin-bottom: 1.6rem;
-        flex-shrink: 1;
-        flex-grow: 0;
-        color: ${palette.onSurfaceVariant};
-        ${elevation.surface.level1};
-        ${elevation.shadow.level1};
-      }
-      .card-img__container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        overflow: hidden;
-        height: 15em;
-        background-color: ${palette.surfaceVariant};
-        padding: 24px;
-
-        img {
-          height: 100%;
-          width: auto;
-          object-fit: contain; 
-          object-position: center;
-        }
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(272px, 1fr));
+        gap: 1.6rem;
       }
 
       #nybble-box-project figure {
-        background-color: ${themeMode === 'light' ? palette.surfaceVariant : null};
+        background-color: ${themeMode === 'light'
+          ? palette.surfaceVariant
+          : null};
       }
       .split-shifts {
-        background-color: #D7756B !important;
+        background-color: #d7756b !important;
       }
-
       .card__info {
         padding: 1.6rem;
       }
 
-      ${media.tabletSmall`
-        .card {
-          flex-basis: calc((100% - 1.6rem) / 2);
-        }
-        .card-img__container  img {
-          height: auto;
-          width: 100%;
-        }
-  
-      `} // End of tabletSmall media query
-
-      ${media.TABLET`
+      @media (min-width: ${media.device.tablet}) {
         padding: var(--vertical-padding) ${media.padding.SMALL};
-        .card {
-          flex-basis: calc((100% - 1.6rem) / 2);
-        }
-      `} // End of TABLET media query
+      }
 
-      ${media.DESKTOP`
+      @media (min-width: ${media.device.desktop}) {
         padding: var(--vertical-padding) ${media.padding.LARGE};
-        
         h2 {
           ${typography.headline.large};
         }
         .card-container {
-          justify-content: flex-start;
+          grid-template-columns: repeat(auto-fit, minmax(312px, 1fr));
         }
-        .card {
-          flex-basis: calc((100% - 3.2rem) / 3);
-          margin-right: 1.6rem;
-        }
-        .card:nth-child(3n) {
-          margin-right: 0;
-        }
-
-      `}// End of DESKTOP media query
+      }
     `;
   }
 );
