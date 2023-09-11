a5354c29ed1e1355dd5c855b4c4b7632490cb408
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-09-11 03:38:43 -0700

    refactor: Remove state import from dark and light themes
    Removed the state import from both the darkTheme.js and lightTheme.js files.

--- california-dev/src/themes/light/lightTheme.js
+++ california-dev/src/themes/light/lightTheme.js
@@ -1,7 +1,6 @@
 import { palette } from './palette';
 import { elevation } from './elevation';
 import { typography } from './typography';
-import { state } from './state';
 import { media } from '../utils/mediaQueries';
 import profileImg from 'assets/images/profile/profile-light_no-bg.png';
 
@@ -10,7 +9,6 @@ export const lightTheme = {
   palette,
   elevation,
   typography,
-  state,
   media,
   profileImg,
 };
