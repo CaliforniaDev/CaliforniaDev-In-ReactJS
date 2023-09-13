662f5d635fccd9d7c085ebdc77f1047aa805cc49
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-09-13 06:22:05 -0700

    feat: Add StateLayer to IconButton component for opacity states
    This commit adds a StateLayer component to the IconButton component to manage different opacity states like hover, focus, and active. The StateLayer component inherits its background color from the parent component's text color, ensuring color consistency. This modular approach simplifies the main component's CSS and increases reusability.
    
    The changes also include simplifying the styles for the IconButton by removing unnecessary CSS properties and using variables from the theme object. The box-shadow and background-color properties are now set using the elevation and palette variables, respectively.
    
    Please review and let me know if there are any issues or concerns.

--- california-dev/src/components/ui/IconButton/IconButton.styles.js
+++ california-dev/src/components/ui/IconButton/IconButton.styles.js
@@ -1,54 +1,54 @@
 import styled, { css } from 'styled-components';
 import { createDynamicButton } from '../utils/buttonUtils';
-const defaultButtonStyling = ({ palette, typography }) => css`
-  display: inline-flex;
-  /* -webkit-tap-highlight-color: transparent; */
+const defaultButtonStyling = ({ palette }) => css`
+  position: relative;
+
+  display: flex;
   justify-content: center;
   align-items: center;
+
   width: 4rem;
   height: 4rem;
-  outline-color: transparent;
   border: none;
   border-radius: 100px;
-  cursor: pointer;
+  outline-color: transparent;
+  overflow: hidden;
+
   color: ${palette.onTertiary};
   background: ${palette.tertiary};
+
+  cursor: pointer;
   transition:
     background-color 0.3s ease-in-out,
     box-shadow 0.3s ease-in-out;
-  position: relative;
-  overflow: hidden;
 
   &:focus-visible {
     outline: none;
   }
-`;
-// ::after Pseudo-element Transition Styling for Button STATE-OVERLAY-COLOR
-const afterPseudoElementStyling = css`
-  &::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    transition:
-      background-color 0.3s ease,
-      opacity 0.3s ease;
-    pointer-events: none;
-    opacity: 0;
-  }
+  /* -webkit-tap-highlight-color: transparent; */
 `;
 export const DefaultIconBtn = createDynamicButton(
   styled.button(
     ({ theme }) => css`
       ${defaultButtonStyling(theme)}
-      ${afterPseudoElementStyling}
+
+      .icon-wrapper {
+        position: relative;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        pointer-events: none;
+      }
+      .state-layer {
+        background-color: ${theme.palette.tertiary};
+      }
     `
   )
 );
 
 export const TargetAreaWrapper = styled.div`
+  position: relative;
   display: inline-flex;
   width: 4.8rem;
   height: 4.8rem;
@@ -57,105 +57,90 @@ export const TargetAreaWrapper = styled.div`
 `;
 
 export const FilledBtn = styled(DefaultIconBtn)(({
-  theme: { palette, elevation, state },
+  theme: { palette, elevation },
 }) => {
-  return css`
-    // Selects svg path
-    #vector {
-      fill: ${palette.onPrimary};
-    }
-    background: ${palette.primary};
-    ${elevation.shadow.level0};
+  const CONTENT_COLOR = palette.onPrimary;
 
+  /** Main component styles */
+  return css`
+    color: ${CONTENT_COLOR};
+    background: ${palette.primary};
+    box-shadow: ${elevation.level0};
+
+    /**
+  * * Keep selector specificity low to allow for
+  * * overriding of styles by the user
+  * * svg - icon
+   */
+    svg path {
+      fill: ${CONTENT_COLOR};
+    }
+    .state-layer {
+      background-color: ${CONTENT_COLOR};
+    }
     &:hover {
-      ${elevation.shadow.level1};
-      &::after {
-        background-color: ${state.onPrimary.hover};
-        opacity: 1;
-      }
+      box-shadow: ${elevation.level1};
     }
     &:focus-visible {
-      ${elevation.shadow.level0};
-
-      &::after {
-        background-color: ${state.onPrimary.focus};
-        opacity: 1;
-      }
+      box-shadow: ${elevation.level1};
     }
     &:active {
-      ${elevation.shadow.level0};
-      &::after {
-        background-color: ${state.onPrimary.pressed};
-        opacity: 1;
-      }
+      box-shadow: ${elevation.level0};
     }
   `;
 });
 
 export const TonalBtn = styled(DefaultIconBtn)(({
-  theme: { palette, elevation, state },
+  theme: { palette, elevation },
 }) => {
+  const CONTENT_COLOR = palette.onSecondaryContainer;
+
+  /** Main component styles */
   return css`
-    // Selects svg path
-    #vector {
-      fill: ${palette.onSecondaryContainer};
-    }
     background: ${palette.secondaryContainer};
-    ${elevation.shadow.level0};
+    box-shadow: ${elevation.level0};
+    svg path {
+      fill: ${CONTENT_COLOR};
+    }
+    .state-layer {
+      background-color: ${CONTENT_COLOR};
+    }
     &:hover {
-      ${elevation.shadow.level1};
-      &::after {
-        background-color: ${state.onSecondaryContainer.hover};
-        opacity: 1;
-      }
+      box-shadow: ${elevation.level1};
     }
     &:focus-visible {
-      ${elevation.shadow.level0};
-      &::after {
-        background-color: ${state.onSecondaryContainer.focus};
-        opacity: 1;
-      }
+      box-shadow: ${elevation.level1};
     }
     &:active {
-      ${elevation.shadow.level0};
-      &::after {
-        background-color: ${state.onSecondaryContainer.pressed};
-        opacity: 1;
-      }
+      box-shadow: ${elevation.level0};
     }
   `;
 });
 
 export const OutlinedBtn = styled(DefaultIconBtn)(({
-  theme: { palette, elevation, state },
+  theme: { palette, elevation },
 }) => {
+  const CONTENT_COLOR = palette.primary;
+
+  /** Main component styles */
   return css`
-    // Selects svg path
-    #vector {
-      fill: ${palette.primary};
-    }
-    background: ${palette.surface};
     border-width: 0.1rem;
     border-style: solid;
     border-color: ${palette.outline};
-    ${elevation.level0};
-    &:hover::after {
-      background-color: ${state.primary.hover};
-      opacity: 1;
+    background: ${palette.surface};
+    box-shadow: ${elevation.level0};
+
+    svg path {
+      fill: ${CONTENT_COLOR};
+    }
+    .state-layer {
+      background-color: ${CONTENT_COLOR};
     }
     &:focus-visible {
       border-color: ${palette.primary};
-      &::after {
-        background-color: ${state.primary.focus};
-        opacity: 1;
-      }
     }
     &:active {
       border-color: ${palette.outline};
-      &::after {
-        background-color: ${state.primary.pressed};
-        opacity: 1;
-      }
     }
   `;
 });
