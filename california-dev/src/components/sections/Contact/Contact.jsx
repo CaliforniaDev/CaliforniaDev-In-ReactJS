e8512d0515e02d4a30ad8f37c20dd153c8bb1096
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-09-16 16:54:08 -0700

    feat: Integrate form validation and controlled components in Contact section
    The Contact form has been refactored to integrate the useValidation
    hook and the validateContactForm utility function. This ensures that
    all form fields are validated before submission.
    
    The form fields are now controlled components, driven by React's
    state management. Error states are dynamically populated from the
    useValidation hook, thereby enhancing user experience and error
    reporting.
    
    Additionally, the form layout and SVG decorations have been refactored
    into separate functional components for improved readability and
    modularity.

--- california-dev/src/components/sections/Contact/Contact.jsx
+++ california-dev/src/components/sections/Contact/Contact.jsx
@@ -1,38 +1,92 @@
+import { useValidation } from 'hooks/useValidation';
+import { validateContactForm } from './utils/validateContactForm';
+
 import { ContactSection, RadioButtonFieldset } from './Contact.styles';
 import { TextField } from 'components/ui/TextField';
 import { Button } from 'components/ui/Button';
 import { RadioButton } from 'components/ui/RadioButton';
+
 import {
   DotPattern,
   ZigZagPattern,
   EclipsePattern,
 } from 'assets/images/shapes';
 
+const SVGDecoration = () => {
+  return (
+    <div className="decoration-container">
+      <DotPattern className="dots-svg top" />
+      <ZigZagPattern className="zigzag-svg" />
+      <EclipsePattern className="eclipse-svg" />
+      <DotPattern className="dots-svg bottom" />
+    </div>
+  );
+};
+const FormHeader = () => {
+  return (
+    <header>
+      <h2>Contact Us</h2>
+      <p className="contact-subtext">
+        Any question or remarks? <span>Just write us a message below!</span>
+      </p>
+    </header>
+  );
+};
+
 export const Contact = () => {
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log('Form submitted!');
-  }
+  const initialState = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    inquiryType: '',
+    message: '',
+  };
+
+  const { formData, formErrors, handleChange, handleSubmit } = useValidation(initialState, validateContactForm);
   return (
     <ContactSection id="contact-us-section">
-      <div className="decoration-container">
-        <DotPattern className="dots-svg top" />
-        <ZigZagPattern className="zigzag-svg" />
-        <EclipsePattern className="eclipse-svg" />
-        <DotPattern className="dots-svg bottom" />
-      </div>
+      <SVGDecoration />
+
       <form onSubmit={handleSubmit} className="contact-form">
-        <header>
-          <h2>Contact Us</h2>
-          <p className="contact-subtext">
-            Any question or remarks? <span>Just write us a message below!</span>
-          </p>
-        </header>
+        <FormHeader />
         <fieldset className="field-grid">
-          <TextField type="text" label="First Name" />
-          <TextField type="text" label="Last Name" />
-          <TextField type="text" label="Email" />
-          <TextField type="text" label="Phone Number" />
+          <TextField
+            type="text"
+            name="firstName"
+            label="First Name"
+            value={formData.firstName}
+            error={formErrors.firstName}
+            id="firstName"
+            onChange={handleChange}
+          />
+          <TextField
+            type="text"
+            name="lastName"
+            label="Last Name"
+            value={formData.lastName}
+            error={formErrors.lastName}
+            id="lastName"
+            onChange={handleChange}
+          />
+          <TextField
+            type="text"
+            name="email"
+            label="Email"
+            value={formData.email}
+            error={formErrors.email}
+            id="email"
+            onChange={handleChange}
+          />
+          <TextField
+            type="text"
+            name="phoneNumber"
+            label="Phone Number"
+            value={formData.phoneNumber}
+            error={formErrors.phoneNumber}
+            id="phoneNumber"
+            onChange={handleChange}
+          />
         </fieldset>
 
         <RadioButtonFieldset>
@@ -41,24 +95,44 @@ export const Contact = () => {
             label="Hiring Inquiry"
             name="inquiryType"
             value="hiring"
+            id="inquiryTypeHiring"
+            checked={formData.inquiryType === 'hiring'}
+            onChange={handleChange}
           />
-          <RadioButton label="Feedback" name="inquiryType" value="feedback" />
+          <RadioButton
+            label="Feedback"
+            name="inquiryType"
+            value="feedback"
+            id="inquiryTypeFeedback"
+            checked={formData.inquiryType === 'feedback'}
+            onChange={handleChange}
+          />
           <RadioButton
             label="Collaboration Opportunities"
             name="inquiryType"
             value="collaboration"
+            id="inquiryTypeCollaboration"
+            checked={formData.inquiryType === 'collaboration'}
+            onChange={handleChange}
           />
           <RadioButton
             label="Technical Support"
             name="inquiryType"
             value="technical"
+            id="inquiryTypeTechnical"
+            checked={formData.inquiryType === 'technical'}
+            onChange={handleChange}
           />
         </RadioButtonFieldset>
 
         <TextField
           multiline
-          className="form-message"
           label="Write your message..."
+          name="message"
+          className="form-message"
+          value={formData.message}
+          id="message"
+          onChange={handleChange}
         />
         <Button
           type="submit"
