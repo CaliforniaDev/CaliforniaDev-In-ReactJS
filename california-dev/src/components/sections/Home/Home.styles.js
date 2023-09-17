688c16d8668636933874190fed2da840c8ad6e6a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-09-16 18:04:03 -0700

    refactor: Reorganize media query in StyledSection component for readability

--- california-dev/src/components/sections/Home/Home.styles.js
+++ california-dev/src/components/sections/Home/Home.styles.js
@@ -36,14 +36,13 @@ export const StyledSection = styled.section(
       .zigzag-svg {
         display: none;
       }
-
-      ${media.DESKTOP`
+      @media (min-width: ${media.device.desktop}) {
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
-        max-width: 1440px; //! CHeck this
-    
-        gap: 3.2rem; //! Check this
+        margin-inline: auto;
+        max-width: var(--desktop-max-width);
+        gap: 3.2rem; 
         padding: ${media.padding.LARGE};
         min-height: auto;
 
@@ -64,82 +63,86 @@ export const StyledSection = styled.section(
           height: 44.8rem;
         }
         .profile-img {
-            z-index: 1; // !Check this
-            display: block;
-            width: inherit;
-            height: inherit;
+          z-index: 1; // !Check this
+          display: block;
+          width: inherit;
+          height: inherit;
 
-            background-color: ${palette.primary};
-            background-size: cover;
-            background-position-x: center;
-            background-image: url(${profileImg});
-            box-shadow: ${elevation.level5};
+          background-color: ${palette.primary};
+          background-size: cover;
+          background-position-x: center;
+          background-image: url(${profileImg});
+          box-shadow: ${elevation.level5};
 
-            z-index: 1;
-          }
-          .outline-frame {
-            position: absolute;
-            width: inherit;
-            height: inherit;
-            border: 2px solid ${palette.primary};
-            left: -20px;
-            top: -20px;
-          }
-          .filled-frame {
-            position: absolute;
-            width: inherit;
-            height: inherit;
-            opacity: 0.8;
-            backdrop-filter: blur(100px);
+          z-index: 1;
+        }
+        .outline-frame {
+          position: absolute;
+          width: inherit;
+          height: inherit;
+          border: 2px solid ${palette.primary};
+          left: -20px;
+          top: -20px;
+        }
+        .filled-frame {
+          position: absolute;
+          width: inherit;
+          height: inherit;
+          opacity: 0.8;
+          backdrop-filter: blur(100px);
 
-            background-color: ${palette.secondary};
-            right: -20px;
-            bottom: -20px;
-          }
+          background-color: ${palette.secondary};
+          right: -20px;
+          bottom: -20px;
+        }
 
         .corner-svg {
-            display: block;
-            position: absolute;
-          }
+          display: block;
+          position: absolute;
+        }
 
-          .top-left {
-            top: -61px;
-            left: -61px;
-            circle {
-              fill: ${palette.primary};
-            }
+        .top-left {
+          top: -61px;
+          left: -61px;
+          circle {
+            fill: ${palette.primary};
           }
-          .bottom-right {
-            bottom: -61px;
-            right: -61px;
-            circle {
-              fill: ${palette.secondary};
-            }
+        }
+        .bottom-right {
+          bottom: -61px;
+          right: -61px;
+          circle {
+            fill: ${palette.secondary};
           }
+        }
 
-          .zigzag-svg {
-            display: block;
-            position: absolute;
+        .zigzag-svg {
+          display: block;
+          position: absolute;
+        }
+        .zigzag-svg.secondary {
+          right: -140px;
+          top: 216px;
+          path {
+            stroke: ${palette.secondary};
           }
-          .zigzag-svg.secondary {
-            right: -140px;
-            top: 216px;
-            path { stroke: ${palette.secondary};}
+        }
+        .zigzag-svg.primary {
+          top: 16px;
+          right: -56px;
+          transform: rotate(-45deg);
+          path {
+            stroke: ${palette.primary};
           }
-          .zigzag-svg.primary {
-            top: 16px;
-            right: -56px; 
-            transform: rotate(-45deg);
-            path {stroke: ${palette.primary};}
+        }
+        .zigzag-svg.small {
+          top: 24px;
+          path {
+            stroke: ${palette.primary};
           }
-          .zigzag-svg.small {
-            top: 24px;
-            path {stroke: ${palette.primary};}
-            pointer-events: none;
-            
-          }
-         
-      `}
+          pointer-events: none;
+        }
+      }
     `;
   }
 );
