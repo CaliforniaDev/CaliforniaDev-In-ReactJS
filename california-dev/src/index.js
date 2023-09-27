bf7b8095fc19d93af42e938bbeeee3d07b492dcc
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-09-27 03:17:49 -0700

    feat(routing): Wrap App with BrowserRouter
    Changes:
    - Introduced the `BrowserRouter` from `react-router-dom` and wrapped the `App` component with it.
    
    This change ensures proper routing behavior for the application, allowing seamless navigation between different components and pages.

--- california-dev/src/index.js
+++ california-dev/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ErrorBoundary from './ErrorBoundary';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ErrorBoundary>
   </React.StrictMode>
 );
