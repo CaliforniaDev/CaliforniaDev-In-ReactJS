209fe6ab16940f4d2e7a7ac60cfcade99fb7564e
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-09-27 03:18:49 -0700

    feat(routing): Add ProjectDetails route, 404 fallback, and simplify router setup
    Changes:
    - Removed the external `Router` wrapper, relying on internal `Routes` to handle routing.
    - Introduced a new route `/projects/:id` to display individual project details using the `ProjectDetails` component.
    - Added a catch-all route (`*`) for unmatched paths, displaying a 404 message.
    
    These enhancements streamline the router setup and expand the app's routing capabilities to better handle different navigation scenarios.

--- california-dev/src/App.jsx
+++ california-dev/src/App.jsx
@@ -1,21 +1,21 @@
 import { ThemeContextProvider } from 'context/ThemeContext';
 import { MainBody } from 'components/Main';
 import { FloatingActionButton } from 'components/ui/FloatingActionButton/FloatingActionButton';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Sandbox} from 'utils/Sandbox/Sandbox';
-
+import { Routes, Route } from 'react-router-dom';
+import { Sandbox } from 'utils/Sandbox/Sandbox';
+import { ProjectDetails } from 'pages/ProjectDetails';
 
 function App() {
   return (
-    <Router>
-      <ThemeContextProvider>
-        <Routes>
-          <Route exact path="/" element={<MainBody />} />
-          <Route path="/sandbox" element={<Sandbox />} />
-        </Routes>
-        <FloatingActionButton />
-      </ThemeContextProvider>
-    </Router>
+    <ThemeContextProvider>
+      <Routes>
+        <Route exact path="/" element={<MainBody />} />
+        <Route path="/sandbox" element={<Sandbox />} />
+        <Route path="/projects/:id" element={<ProjectDetails />} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
+      <FloatingActionButton />
+    </ThemeContextProvider>
   );
 }
 
