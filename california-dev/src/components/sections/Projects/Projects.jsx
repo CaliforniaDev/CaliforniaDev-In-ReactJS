333831b6252ca0ef703481931a3ee515fa6e7502
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-09-27 03:30:23 -0700

    feat(Projects): Add links to individual project details
    Changes:
    - Imported `Link` from `react-router-dom` to enable navigation to individual project detail pages.
    - Wrapped each `Card` component within a `Link` to allow users to click through to the detailed view of the project.
    - The link's path is dynamically generated based on the project's ID, directing to the corresponding `ProjectDetails` page.
    
    With these modifications, users can now easily navigate from the list of projects to a detailed page for each individual project.

--- california-dev/src/components/sections/Projects/Projects.jsx
+++ california-dev/src/components/sections/Projects/Projects.jsx
@@ -2,6 +2,7 @@
 import { ProjectSection } from './Projects.styles';
 import { projectData } from './data/projectData';
 import { useTheme } from 'context/ThemeContext';
+import { Link } from 'react-router-dom';
 
 import { Card } from 'components/ui/Card/Card';
 
@@ -10,25 +11,27 @@ export const Projects = () => {
 
   const renderProjects = () => {
     return projectData.map(project => (
-      <Card
-        id={project.id}
-        key={project.id}
-        title={project.title}
-        projectType={project.projectType}
-        ImageComponent={project.imageComponent}
-        src={project.src}
-        backgroundColor={project.backgroundColor}
-      />
+      <Link to={`/projects/${project.id}`} key={project.id}>
+        <Card
+          id={project.id}
+          title={project.title}
+          projectType={project.projectType}
+          ImageComponent={project.imageComponent}
+          src={project.src}
+          backgroundColor={project.backgroundColor}
+        />
+      </Link>
     ));
   };
   return (
-    <ProjectSection themeMode={themeMode} id="projects-section" aria-label="Recent Projects">
+    <ProjectSection
+      themeMode={themeMode}
+      id="projects-section"
+      aria-label="Recent Projects"
+    >
       <div className="content-container">
         <h2 id="projects-heading">-Recent Projects-</h2>
-        <ul
-          className="card-container"
-          aria-labelledby="projects-heading"
-        >
+        <ul className="card-container" aria-labelledby="projects-heading">
           {renderProjects()}
         </ul>
       </div>
