b069f12e4743976d87ba40849130dfb09cd18f25
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-10-04 05:00:40 -0700

    refactor(App): Update to use React Router v6.4 route configuration
    Changes:
    - Adopted the new route object configuration style introduced in React Router v6.4.
    - Migrated from <Routes> and <Route> to `createBrowserRouter` and `RouterProvider`.
    - Streamlined route definitions into a centralized routes array, making the App component cleaner.
    
    This update aligns our routing strategy with the latest React Router best practices and enhances maintainability.

--- california-dev/src/index.js
+++ california-dev/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ErrorBoundary from './ErrorBoundary';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </ErrorBoundary>
   </React.StrictMode>
 );
