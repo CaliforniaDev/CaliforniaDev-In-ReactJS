7fe9411a6208b90cc0807ac4becbb5ac847d09db
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-10-11 03:25:35 -0700

    enhance: Spread props to component

--- california-dev/src/components/Navigation/Nav.jsx
+++ california-dev/src/components/Navigation/Nav.jsx
@@ -1,9 +1,9 @@
 import * as Styled from './NavStyles';
 import { Logo } from 'assets/images/logo/Logo';
 import { MenuButton } from '../ui/MenuButton';
-export function Nav() {
+export function Nav({ ...props }) {
   return (
-    <Styled.Nav>
+    <Styled.Nav {...props}>
       <div id="wrapper">
         <Logo className="logo" alt="logo image" />
         <MenuButton />
