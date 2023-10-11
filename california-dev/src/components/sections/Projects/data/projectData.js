322502f10f4f6d502e2eb9312ca5bfe58abd4ee1
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-10-11 05:00:29 -0700

    refactor(projectData): utilize class structure for project definition
    Transformed the previous plain objects used to define projects into
    instances of a new 'Project' class. This ensures a consistent data
    structure for each project and simplifies the process of adding or
    modifying projects in the future. Additional detailed descriptions and
    links were also added for some projects.

--- california-dev/src/components/sections/Projects/data/projectData.js
+++ california-dev/src/components/sections/Projects/data/projectData.js
@@ -7,90 +7,161 @@ import {
 
 import cupkayzHomeScreen from 'assets/images/projects/cupkayz-webpage/cupkayz_home-screen@3x.png';
 import memoryGamePreview from 'assets/images/projects/memory-game/memory-game@3x.png';
+import nybbleBoxPreview from 'assets/images/projects/nybblebox/nybblebox-webpage@3x.png';
+
+class Project {
+  constructor({
+    id,
+    title,
+    client,
+    projectType,
+    imageComponent,
+    src,
+    detailedImgSrc,
+    techStack,
+    description,
+    repository,
+    livePreview,
+    backgroundColor,
+  }) {
+    this.id = id;
+    this.title = title;
+    this.client = client;
+    this.projectType = projectType;
+    this.imageComponent = imageComponent;
+    this.src = src;
+    this.backgroundColor = backgroundColor;
+    this.detailedImgSrc = detailedImgSrc;
+    this.techStack = techStack;
+    this.repository = repository;
+    this.livePreview = livePreview;
+    this.description = description;
+  }
+}
+
+const cupKayzProject = new Project({
+  id: 'cup-kayz-project',
+  title: 'CupKayz',
+  client: 'Karla Mendez',
+  projectType: 'Ecommerce Website',
+  imageComponent: CupKayz,
+  backgroundColor: '#778b7c',
+  detailedImgSrc: cupkayzHomeScreen,
+  techStack: ['HTML', 'CSS', 'JavaScript', 'Adobe XD'],
+  repository: 'https://github.com/CaliforniaDev/CupkayzV2.0',
+  livePreview: 'https://californiadev.github.io/CupkayzV2.0/',
+  description: [
+    `CupKayz is a fully responsive ecommerce web application 
+    designed to offer a seamless shopping experience across multiple 
+    devices. The application serves as a one stop shop for a variety 
+    of bakery items, featuring functionalities like product selection, 
+    cart management, and location based delivery services.`,
+
+    `Built using HTML, CSS, and Vanilla JavaScript, the project avoids 
+    reliance on external libraries, ensuring optimal performance and 
+    maintainability. The application integrates Google Maps API for 
+    real-time location tracking to enhance the delivery service. 
+    It also offers an Autocomplete feature for address input, 
+    further enhancing user experience.`,
+
+    `The planning and design phase involved creating low and high fidelity 
+    wireframes using Adobe XD's 8pt grid system, ensuring a consistent and 
+    intuitive UI/UX design. This comprehensive approach demonstrates a deep 
+    understanding of frontend technologies and API integrations, showcasing 
+    the ability to plan, design, and execute a full fledged ecommerce application 
+    with a focus on user experience and performance.`,
+  ],
+});
+
+const nybbleBoxProject = new Project({
+  id: 'nybble-box-project',
+  title: 'NybbleBox',
+  client: 'Personal Project',
+  projectType: 'Landing Page',
+  imageComponent: NybbleBox,
+  backgroundColor: '#0d0d0d',
+  detailedImgSrc: nybbleBoxPreview,
+  techStack: ['WordPress'],
+  repository: null,
+  livePreview: 'http://nybblebox.com/',
+  description: [
+    `NybbleBox is a WordPress-based landing page designed to serve as 
+      the digital front door for a branding agency specializing in small 
+      businesses. The project embodies the company's mission to make 
+      powerful branding accessible to everyone, regardless of their skill 
+      level, design background, or budget.`,
+
+    `The landing page is crafted to streamline the branding process, 
+      offering a range of services and solutions that can elevate any 
+      business's story into a compelling brand. It serves as a one-stop 
+      platform where potential clients can learn about the services offered, 
+      view portfolio pieces, and get in touch for customized solutions.`,
+
+    `Built on WordPress, the project leverages the platform's robust 
+      features and flexibility, allowing for easy content updates and 
+      scalability. The design is fully responsive, ensuring a seamless user 
+      experience across various devices. SEO best practices were also 
+      implemented to improve the site's visibility and reach a wider audience.`,
+  ],
+});
+
+const memoryGameProject = new Project({
+  id: 'memory-game-project',
+  title: 'Memory Game',
+  client: 'Personal Project',
+  projectType: 'React App',
+  imageComponent: null, // svgThumbnail,
+  src: memoryGameSrc, // change the key name to staticThumbnail
+  backgroundColor: null,
+  detailedImgSrc: memoryGamePreview,
+  techStack: ['HTML', 'CSS', 'JavaScript', 'React'],
+  repository: 'https://github.com/CaliforniaDev/React-Memory-Game',
+  livePreview: 'https://californiadev.github.io/React-Memory-Game/',
+  description: [
+    `The React Memory Game is an interactive web-based game designed to 
+    challenge players' memory skills. Built using ReactJS, the game offers 
+    a dynamic and engaging user experience, complete with a real-time scoreboard, 
+    multiple levels, and a game-over screen.`,
+
+    `The game leverages custom React hooks to encapsulate game logic, 
+    card management, and scoring, making the codebase modular and easier to maintain. 
+    Styled-components are used for component-level styling, enhancing reusability and maintainability. 
+    The project also employs Error Boundaries for graceful error handling.`,
+
+    `Key features include a dynamic game board that adjusts according to the level, 
+    a real-time scoreboard, a game-over screen with performance metrics, an instruction 
+    modal for new players, and a loading screen for initial data loading. The codebase follows 
+    industry best practices, including componentization and centralized theming for easier UI updates 
+    and a consistent look and feel.`,
+  ],
+});
+
+const splitShiftsProject = new Project({
+  id: 'split-shifts-project',
+  title: 'SplitShifts',
+  client: 'BW Security Services Inc.',
+  projectType: 'Schedule Management Web App',
+  imageComponent: SplitShifts, // svgThumbnail,
+  src: null, // change the key name to staticThumbnail
+  backgroundColor: '#d7756b',
+  detailedImgSrc: null,
+  techStack: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node.js',
+    'Express',
+    'MongoDB',
+  ],
+  repository: null,
+  livePreview: null,
+  description: null,
+});
 
 export const projectData = [
-  {
-    id: 'cup-kayz-project',
-    title: 'CupKayz',
-    client: 'Karla Mendez',
-    projectType: 'Ecommerce Website',
-    imageComponent: CupKayz,
-    backgroundColor: '#778b7c',
-    techStack: ['HTML', 'CSS', 'JavaScript', 'Adobe XD'],
-    detailedImgSrc: cupkayzHomeScreen,
-
-    description: [
-      `As a skilled web developer, I built a 
-      responsive ecommerce web application that can be 
-      used on various devices. To improve delivery service, 
-      I integrated the Google Maps API for map locations, 
-      and added the Autocomplete feature for address input for 
-      enhanced user experience.`,
-
-      `To plan out the design, I created 
-      low and high fidelity wire-frames using Adobe XD's 8pt grid system. 
-      I also used vanilla JavaScript, ensuring the project was completed 
-      without reliance on any libraries. This approach helped me to have 
-      a better control and maintainability of the final solution.`,
-    ],
-  },
-  {
-    id: 'nybble-box-project',
-    title: 'NybbleBox',
-    client: 'Personal Project',
-    projectType: 'Landing Page',
-    imageComponent: NybbleBox,
-    src: null,
-    techStack: ['WordPress'],
-    backgroundColor: '#0d0d0d',
-  },
-
-  {
-    id: 'memory-game-project',
-    title: 'Memory Game',
-    client: 'Personal Project',
-    projectType: 'React App',
-    imageComponent: null,
-    src: memoryGameSrc,
-    detailedImgSrc: memoryGamePreview,
-    repository: 'https://github.com/CaliforniaDev/React-Memory-Game',
-    livePreview: 'https://californiadev.github.io/React-Memory-Game/',
-    backgroundColor: null,
-    techStack: ['HTML', 'CSS', 'JavaScript', 'React'],
-    description: [
-      `The React Memory Game is an interactive web-based game designed to 
-      challenge players' memory skills. Built using ReactJS, the game offers 
-      a dynamic and engaging user experience, complete with a real-time scoreboard, 
-      multiple levels, and a game-over screen.`,
-
-      `The game leverages custom React hooks to encapsulate game logic, 
-      card management, and scoring, making the codebase modular and easier to maintain. 
-      Styled-components are used for component-level styling, enhancing reusability and maintainability. 
-      The project also employs Error Boundaries for graceful error handling.`,
-
-      `Key features include a dynamic game board that adjusts according to the level, 
-      a real-time scoreboard, a game-over screen with performance metrics, an instruction 
-      modal for new players, and a loading screen for initial data loading. The codebase follows 
-      industry best practices, including componentization and centralized theming for easier UI updates 
-      and a consistent look and feel.`,
-    ],
-  },
-  {
-    id: 'split-shifts-project',
-    title: 'SplitShifts',
-    client: 'BW Security Services Inc.',
-    projectType: 'Schedule Management Web App',
-    imageComponent: SplitShifts,
-    techStack: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-      'React',
-      'Node.js',
-      'Express',
-      'MongoDB',
-    ],
-    src: null,
-    backgroundColor: '#d7756b',
-  },
+  cupKayzProject,
+  nybbleBoxProject,
+  memoryGameProject,
+  splitShiftsProject,
 ];
