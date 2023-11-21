26b349425d90445c8f9d1da34debc703019981ee
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-11-21 04:54:51 -0800

    Integrate Framer Motion animations and forwardRef in Projects component
    This commit introduces two major updates to the Projects component:
    - Framer Motion Integration: The Projects component now utilizes Framer Motion
      to animate individual project cards and the overall list. This includes
      defining 'listContainer' and 'item' variants for staggered animations. Each
      project card now fades in and moves upwards as it becomes visible, creating
      a more dynamic and engaging user experience.
    - React.forwardRef Usage: The component is now wrapped with React.forwardRef,
      enabling parent components to directly reference the Projects component's DOM
      node. This enhancement is particularly beneficial for interactions requiring
      precise control over the Projects component's position or state.
    
    Overall, these changes significantly enhance the visual appeal and functionality
    of the Projects section, making it more interactive and accessible.

--- california-dev/src/components/sections/Projects/Projects.jsx
+++ california-dev/src/components/sections/Projects/Projects.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 // Local imports
 import { ProjectSection } from './Projects.styles';
 import { projectData } from './data/projectData';
@@ -6,34 +7,75 @@ import { Link } from 'react-router-dom';
 
 import { Card } from 'components/ui/Card/Card';
 
-export const Projects = () => {
+import { motion } from 'framer-motion';
+
+export const Projects = React.forwardRef((props, ref) => {
   const { themeMode } = useTheme();
 
-  const projectsList = projectData.map(project => (
-    <Link to={`/projects/${project.id}`} key={project.id}>
-      <Card
-        id={project.id}
-        title={project.title}
-        projectType={project.projectType}
-        ImageComponent={project.svgThumbnail}
-        src={project.staticThumbnail}
-        backgroundColor={project.thumbnailBackground}
-      />
-    </Link>
-  ));
+  // Framer motion variants
+  const listContainer = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0,
+        staggerChildren: 0.3,
+        when: 'beforeChildren',
+      },
+    },
+  };
+
+  // Framer motion variants
+  const item = {
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0 },
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      mass: 0.3,
+      delay: 2,
+    },
+  };
+
+  const projectsList = projectData.map(project => {
+    return (
+      <motion.li variants={item} key={project.id}>
+        <Link to={`/projects/${project.id}`}>
+          <Card
+            id={project.id}
+            title={project.title}
+            projectType={project.projectType}
+            ImageComponent={project.svgThumbnail}
+            src={project.staticThumbnail}
+            backgroundColor={project.thumbnailBackground}
+          />
+        </Link>
+      </motion.li>
+    );
+  });
 
   return (
     <ProjectSection
+      ref={ref}
       $themeMode={themeMode}
       id="projects-section"
       aria-label="Recent Projects"
     >
       <div className="content-container">
         <h2 id="projects-heading">-Recent Projects-</h2>
-        <ul className="card-container" aria-labelledby="projects-heading">
+        <motion.ul
+          variants={listContainer}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ threshold: 0.5, once: true }}
+          className="card-container"
+          aria-labelledby="projects-heading"
+        >
           {projectsList}
-        </ul>
+        </motion.ul>
       </div>
     </ProjectSection>
   );
-};
+});
