01ddff1a85933c34ec99bc51593546eb40d92fd2
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-12-04 01:06:49 -0800

    refactor: Enhance MobileNav Component with Accessibility and Simplified Logic
    This commit introduces several enhancements and optimizations to the MobileNav component:
    
    1. **Simplified Display Logic:**
       - Replaced the `useMemo` hook with direct logic for determining the display style of the logo. This simplification reduces the complexity of the component while still providing efficient rendering.
       - Updated the logic for determining the class name of the nav element, streamlining the process of switching between 'fab fixed' and 'top-nav fixed' based on the `isFab` state.
    
    2. **Accessibility Improvements:**
       - Added `role` and `aria-label` attributes to the `TargetArea` component, enhancing accessibility by making the navigation menu more screen-reader friendly.
    
    3. **Animation Variant Refactor:**
       - Refactored the animation variants for the logo (`logoAnimationVariants`) and the theme button (`themeButtonAnimationVariants`) into standalone constants outside the component, improving readability and maintainability.
    
    4. **Maintained Core Functionality:**
       - All other core functionalities of the MobileNav component, including theme toggling, menu handling, and responsive design, remain intact and unaltered.
    
    These refinements to the MobileNav component enhance its accessibility, simplify its internal logic, and maintain its existing features, thereby improving both the developer experience and the end-user interface.

--- california-dev/src/components/ui/Navigation/Mobile/MobileNav.jsx
+++ california-dev/src/components/ui/Navigation/Mobile/MobileNav.jsx
@@ -1,6 +1,4 @@
 import { motion } from 'framer-motion';
-
-import { useMemo } from 'react';
 import { useTheme } from 'context/ThemeContext';
 import { useMobileNav } from './hooks/useMobileNav';
 
@@ -16,6 +14,46 @@ import { NavMenu } from './NavMenu';
 import { Nav, ThemeButton, TargetArea } from './MobileNav.styles';
 import { MoonIcon, SunIcon } from 'assets/images/icons/navigation';
 
+// Variants for the logo animation
+const logoAnimationVariants = {
+  open: {
+    x: -24,
+    opacity: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.5,
+    },
+  },
+  closed: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      delay: 0.5,
+    },
+  },
+};
+
+const themeButtonAnimationVariants = {
+  open: {
+    opacity: 1,
+    display: 'flex',
+    y: 0,
+    transition: {
+      delay: 0.5,
+    },
+  },
+  closed: {
+    opacity: 0,
+    y: 40,
+    transition: {
+      delay: 0.5,
+    },
+    transitionEnd: {
+      display: 'none',
+    },
+  },
+};
 
 export const MobileNav = ({ scrollYProgress, ...props }) => {
   const { toggleTheme } = useTheme();
@@ -23,72 +61,26 @@ export const MobileNav = ({ scrollYProgress, ...props }) => {
   const { width, logoOpacity, isFab, isMenuOpen, handleMenuToggle } =
     useMobileNav(scrollYProgress);
 
-  // Variants for the logo animation
-  const logoVariants = {
-    open: {
-      x: -24,
-      opacity: 0,
-      transition: {
-        type: 'spring',
-        delay: 0.5,
-      },
-    },
-    closed: {
-      x: 0,
-      opacity: 1,
-      transition: {
-        type: 'spring',
-        delay: 0.5,
-      },
-    },
-  };
+  // Determine the display style for the logo <span> wrapper
+  const displayStyle = logoOpacity === 0 || isFab ? 'none' : 'block';
 
-  const themeButtonVariants = {
-    open: {
-      opacity: 1,
-      display: 'flex',
-      y: 0,
-      transition: {
-        delay: 0.5,
-      },
-    },
-    closed: {
-      opacity: 0,
-      y: 40,
-      transition: {
-        delay: 0.5,
-      },
-      transitionEnd: {
-        display: 'none',
-      },
-    },
-  };
-
-  const changeDisplayAttr = useMemo(() => {
-    if (logoOpacity === 0 || isFab === true) return 'none';
-    return 'block';
-  }, [logoOpacity, isFab]);
-
-  const isFabOrNav = useMemo(() => {
-    if (isFab === true) return 'fab fixed';
-    if (isFab === false) return 'top-nav fixed';
-  }, [isFab]);
+  // Determine the class for the nav based on isFab
+  const navClass = isFab ? 'fab fixed' : 'top-nav fixed';
   return (
     <Nav
       $isFab={isFab}
-      $isOpen={isMenuOpen}
       initial={false}
       animate={isMenuOpen ? 'open' : 'closed'}
-      className={isFabOrNav}
+      className={navClass}
       style={{ width }}
       layout
       {...props}
     >
       <motion.div
-        variants={logoVariants}
+        variants={logoAnimationVariants}
         className="logo-wrapper"
         style={{
-          display: changeDisplayAttr,
+          display: displayStyle,
         }}
       >
         <span style={{ opacity: logoOpacity }} className="opacity-on-scroll">
@@ -97,11 +89,10 @@ export const MobileNav = ({ scrollYProgress, ...props }) => {
       </motion.div>
 
       <ThemeButton
-        variants={themeButtonVariants}
+        variants={themeButtonAnimationVariants}
         initial="closed"
         animate={isMenuOpen ? 'open' : 'closed'}
         className="theme-btn"
-        $isFab={isFab}
         onClick={toggleTheme}
       >
         <SunIcon className="theme-icon sun" size="24px" />
@@ -111,11 +102,12 @@ export const MobileNav = ({ scrollYProgress, ...props }) => {
       </ThemeButton>
 
       <TargetArea
-        $isFab={isFab}
+        role="button"
+        aria-label="Open Nav Menu"
         className="target-area"
         onClick={handleMenuToggle}
       >
-        <MenuToggle className="menu-toggle"></MenuToggle>
+        <MenuToggle className="menu-toggle" />
         <StateLayer className="state-layer" />
       </TargetArea>
 
@@ -123,145 +115,3 @@ export const MobileNav = ({ scrollYProgress, ...props }) => {
     </Nav>
   );
 };
-
-// <Nav
-//   className="fixed fab"
-//   variants={variants.slideInFromTop}
-//   initial="hidden"
-//   animate="visible"
-// >
-
-// import React, { useState, useEffect } from 'react';
-// import { motion } from 'framer-motion';
-
-// import { useTheme } from 'context/ThemeContext';
-// import { useMobileNav } from './hooks/useMobileNav';
-// import { useScrollInfo } from 'context/ScrollContext';
-
-// //Assets
-// import { Logo } from 'assets/images/logo/Logo';
-
-// //Components
-// import { StateLayer } from 'components/ui/StateLayer';
-// import { MenuToggle } from './MenuToggle';
-// import { NavMenu } from './NavMenu';
-
-// //Styles and Utilities
-// import { Nav } from './MobileNav.styles';
-// import { motionVariants as variants } from 'components/ui/utils/motionVariants';
-
-// /**
-//  * Custom hook to get the position of the menu button on screen.
-//  */
-// const useMenuButtonPosition = () => {
-//   const { scrollYProgress } = useScrollInfo();
-//   const { navPosition, isFab } = useMobileNav(scrollYProgress);
-//   const PADDING_RIGHT = 20;
-//   const HALF_BUTTON_SIZE = 40 / 2;
-//   const NAVBAR_HEIGHT = 80;
-
-//   const [x, setX] = useState(
-//     window.innerWidth - PADDING_RIGHT - HALF_BUTTON_SIZE
-//   );
-//   const [y, setY] = useState(NAVBAR_HEIGHT / 2 + navPosition);
-
-//   useEffect(() => {
-//     const updatePosition = () => {
-//       setX(window.innerWidth - PADDING_RIGHT - HALF_BUTTON_SIZE);
-//       setY(NAVBAR_HEIGHT / 2 + navPosition);
-//       if (isFab) {
-//         setX(window.innerWidth - PADDING_RIGHT - (HALF_BUTTON_SIZE + 16));
-//         setY(NAVBAR_HEIGHT / 3 + navPosition);
-//       }
-//     };
-//     window.addEventListener('resize', updatePosition);
-//     updatePosition();
-//     return () => {
-//       window.removeEventListener('resize', updatePosition);
-//     };
-//   }, [navPosition, isFab]);
-//   return [x, y];
-// };
-
-// /**
-//  * Computes the radius of the circle needed to cover the full screen.
-//  */
-// const computeFullScreenCircleRadius = () => {
-//   const width = window.innerWidth;
-//   const height = window.innerHeight;
-//   return Math.sqrt(width ** 2 + height ** 2);
-// };
-
-// export const MobileNav = ({ scrollYProgress, className, ...props }) => {
-//   const [isOpen, setIstOpen] = useState(false);
-//   const [x, y] = useMenuButtonPosition();
-//   const { width, logoOpacity, navPosition, isFab } =
-//     useMobileNav(scrollYProgress);
-
-//   // Variants for the menu curtain animation
-//   const menuCurtain = {
-//     open: {
-//       clipPath: `circle(${computeFullScreenCircleRadius()}px at ${x}px ${y}px)`,
-
-//       opacity: 1,
-//       transition: {
-//         type: 'spring',
-//         stiffness: 40,
-//         restDelta: 2,
-//       },
-//     },
-//     closed: {
-//       clipPath: `circle(34px at ${x}px ${y}px)`,
-
-//       transition: {
-//         delay: 0.5,
-//         type: 'spring',
-//         stiffness: 400,
-//         damping: 40,
-//       },
-//     },
-//   };
-
-//   const handleClick = () => {
-//     setIstOpen(!isOpen);
-//   };
-
-//   return (
-//     <Nav
-//       className={`${className} ${isFab ? 'fab' : ''}`}
-//       $isFab={isFab}
-//       initial={false}
-//       animate={isOpen ? 'open' : 'closed'}
-//       style={{ width }}
-//       $isOpen={isOpen}
-//       $logoOpacity={logoOpacity}
-//       $navPosition={navPosition}
-//       layout
-//       {...props}
-//     >
-//       <div id="wrapper">
-//         <Logo className="nav__logo nav-item" alt="logo image" />
-//         <MenuToggle
-//           isOpen={isOpen}
-//           toggle={() => handleClick()}
-//           className="menu-toggle"
-//         />
-//       </div>
-//       <motion.div
-//         className="menu-background"
-//         initial="closed"
-//         animate={isOpen ? 'open' : 'closed'}
-//         variants={menuCurtain}
-//       >
-//         <NavMenu className="nav-menu" />
-//       </motion.div>
-//     </Nav>
-//   );
-// };
-
-// // <Nav
-// //   className="fixed fab"
-// //   variants={variants.slideInFromTop}
-// //   initial="hidden"
-// //   animate="visible"
-// // >
