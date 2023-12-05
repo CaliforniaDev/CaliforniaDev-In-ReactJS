0f3f0015af9cbd4d805094ff5481c58de9e644e2
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-12-05 06:25:50 -0800

    feat: Implement Navigation Context Provider for SPA Navigation Management
    This commit introduces the NavigationProvider component, a comprehensive solution for managing navigation states in SPA environments. Key features include:
    
    - Creation of `NavigationContext` for centralized navigation state management.
    - Implementation of `useNavContext` custom hook for easy access to navigation context.
    - State management for active anchors, pressed anchors, programmatic scroll, menu visibility, and logo display control.
    - Integration with `react-router-dom` for handling default routing and location-based actions.
    - Effects for managing default route handling, updating active anchors, and resetting programmatic scroll state.
    - Global mouse event listener to reset pressed anchor state.
    
    This provider enhances the navigation experience by ensuring smooth transitions, state synchronization, and responsive interactions across the application. It's a significant step towards a more dynamic and interactive SPA.

---
+++ california-dev/src/context/NavigationContext.js
@@ -0,0 +1,118 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useCallback,
+} from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const NavigationContext = createContext();
+
+export const useNavContext = () => {
+  const context = useContext(NavigationContext);
+  if (context === undefined) {
+    throw new Error(
+      'useNavContext must be used within a NavigationProvider component'
+    );
+  }
+  return context;
+};
+
+export const NavigationProvider = ({ children }) => {
+  const [activeAnchor, setActiveAnchor] = useState();
+  const [pressedAnchor, setPressedAnchor] = useState(null);
+  const [isProgrammaticScroll, setIsProgrammaticScroll] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [logoDisplay, setLogoDisplay] = useState('none');
+  const navigate = useNavigate();
+  const location = useLocation();
+  const defaultRoute = '/#home-section';
+
+  /**
+   * Handles default route and scroll behavior on component mount or route change.
+   */
+  useEffect(() => {
+    const { pathname, hash } = location;
+    // Check if there is no hash in the URL if so, navigate to the default route
+    if (pathname === '/' && !hash && defaultRoute) {
+      return navigate(defaultRoute);
+    }
+    // Scroll to the target element if a hash is present
+    if (hash && isProgrammaticScroll) {
+      try {
+        const targetElement = document.querySelector(hash);
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      } catch (error) {
+        console.error('Error scrolling to target element: ', error);
+      }
+    }
+  }, [defaultRoute, navigate, location, isProgrammaticScroll]);
+
+  /**
+   * Updates the active anchor when location hash changes.
+   */
+  const handleActiveAnchorUpdate = () => {
+    setActiveAnchor(location.hash);
+  };
+  useEffect(handleActiveAnchorUpdate, [location.hash]);
+
+  const handleProgrammaticScroll = useCallback(() => {
+    if (isProgrammaticScroll) {
+      const timer = setTimeout(() => {
+        // Reset after each link click
+        setIsProgrammaticScroll(false);
+      }, 500);
+      return () => clearTimeout(timer);
+    }
+  }, [isProgrammaticScroll]);
+  useEffect(handleProgrammaticScroll, [handleProgrammaticScroll]);
+
+  /**
+   * Manages global mouse events to reset pressedAnchor state.
+   * This effect cleans itself up by removing the event listener.
+   */
+  const handleGlobalMouseEvents = useCallback(() => {
+    const handleGlobalMouseUp = () => {
+      setPressedAnchor(null);
+    };
+    window.addEventListener('mouseup', handleGlobalMouseUp);
+    return () => window.removeEventListener('mouseup', handleGlobalMouseUp);
+  }, []);
+  useEffect(handleGlobalMouseEvents, [handleGlobalMouseEvents]);
+
+  const value = {
+    activeAnchor,
+    pressedAnchor,
+    isMenuOpen,
+    isProgrammaticScroll,
+    logoDisplay,
+    setPressedAnchor,
+    setActiveAnchor,
+    setIsMenuOpen,
+    setIsProgrammaticScroll,
+    setLogoDisplay,
+  };
+
+  return (
+    <NavigationContext.Provider value={value}>
+      {children}
+    </NavigationContext.Provider>
+  );
+
+  // return (
+  //   <NavigationContext.Provider
+  //     value={{
+  //       activeAnchor,
+  //       pressedAnchor,
+  //       setPressedAnchor,
+  //       setIsProgrammaticScroll,
+  //     }}
+  //   >
+  //     {children}
+  //   </NavigationContext.Provider>
+  // );
+  //};
+};
