7aab1a5b6b5c20547bd61dcb04d5bf15295e2263
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-12-05 06:38:40 -0800

    refactor: Revise useMobileNav Hook with Navigation Context Integration
    Significant changes and improvements made to the useMobileNav custom hook include:
    
    - Integration with the useNavContext for centralized navigation state management.
    - Removal of redundant state variables such as isFab, logoOpacity, and isMenuOpen, now managed globally.
    - Introduction of useCallback for optimizing event handlers like handleMenuLinkClick and handleMenuToggle.
    - Enhanced handleMenuLinkClick to include smooth scrolling to target elements, and navigation logic based on route.
    - Streamlining of the hook's return values to focus on essential functionalities.
    
    This refactor simplifies the useMobileNav hook by leveraging the NavigationContext, ensuring a more maintainable and cohesive navigation logic across the application.

--- california-dev/src/components/ui/Navigation/Mobile/hooks/useMobileNav.js
+++ california-dev/src/components/ui/Navigation/Mobile/hooks/useMobileNav.js
@@ -1,80 +1,32 @@
-import React, { useState, useEffect, useRef } from 'react';
-import {
-  motion,
-  useScroll,
-  useMotionValueEvent,
-  useMotionValue,
-  useTransform,
-} from 'framer-motion';
+import React, { useState, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useNavContext } from 'context/NavigationContext';
 
-export const useMobileNav = scrollYProgress => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isFab, setIsFab] = useState(false);
-  const [logoOpacity, setLogoOpacity] = useState(1);
-  // const [navPosition, setNavPosition] = useState(0);
+export const useMobileNav = () => {
+  const { setIsMenuOpen,setActiveAnchor, setIsProgrammaticScroll } = useNavContext();
+  const navigate = useNavigate();
 
-  // const menuSize = '100%';
-  // const viewportHeight = window.innerHeight;
-  // const navHeight = 80;
-
-  const frame = {
-    initial: 0,
-    logoFadeOut: 0.001,
-    reachMinWidth: 0.2,
-    startTopPositionChange: 0.3,
-    endTopPositionChange: 0.5,
+  const handleMenuLinkClick = (event, id, route) => {
+    setIsProgrammaticScroll(true);
+    if (id) {
+      const targetElement = document.querySelector(id);
+      if (targetElement) {
+        event.preventDefault();
+        handleMenuToggle();
+        setActiveAnchor(id);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+    
+    return navigate(route);
   };
 
-  useMotionValueEvent(scrollYProgress, 'change', latest => {
-    setIsFab(latest > 0.6);
-  });
-
-  const width = useTransform(
-    scrollYProgress,
-    [frame.initial, frame.reachMinWidth],
-    ['100%', '0%']
-  );
-
-  const opacity = useTransform(
-    scrollYProgress,
-    [frame.initial, frame.logoFadeOut],
-    [1, 0]
-  );
-  useMotionValueEvent(opacity, 'change', latest => {
-    setLogoOpacity(latest);
-  });
-
-  const handleMenuLinkClick = () => {};
-
-  const handleMenuToggle = () => setIsMenuOpen(prevState => !prevState);
+  const handleMenuToggle = () => {
+    setIsMenuOpen(prevState => !prevState);
+  };
 
   return {
-    isMenuOpen,
-    // menuSize,
     handleMenuToggle,
     handleMenuLinkClick,
-    width,
-    // opacity,
-    // position,
-    // display,
-    logoOpacity,
-    // navPosition,
-    isFab,
   };
 };
-
-// const position = useTransform(
-//   scrollYProgress,
-//   [frame.startTopPositionChange, frame.endTopPositionChange],
-//   [0, 1]
-// );
-
-// useMotionValueEvent(position, 'change', latest => {
-//   console.log(latest, "latest");
-//   const fromTop = (viewportHeight - navHeight) * latest;
-//   setNavPosition(fromTop);
-// })
-
-// const display = useTransform(scrollYProgress, pos =>
-//   pos > frame.startTopPositionChange ? 'none' : 'block'
-// );
