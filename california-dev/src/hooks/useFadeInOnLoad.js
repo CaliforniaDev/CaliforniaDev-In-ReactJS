9125f87035068ee438b5f2f64dbaea39892f5cea
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-12-07 01:40:30 -0800

    add: Create useFadeInOnLoad Hook
    This commit introduces the `useFadeInOnLoad` custom React hook into our project. The hook's purpose is to manage a fade-in effect for components when they load.
    
    Key features of the new hook:
    - It leverages React's `useState` and `useEffect` hooks to control the fade-in behavior.
    - The hook initializes with a `false` state for `isLoaded`, indicating that the component is not yet ready for the fade-in effect.
    - A timer is set using `setTimeout` within `useEffect`, which, after the specified delay (defaulting to 500 milliseconds), sets `isLoaded` to `true`.
    - The delay is customizable, allowing different components to have varied fade-in timings.
    - A cleanup function is returned from `useEffect` to clear the timer, ensuring no memory leaks or unintended side effects.
    - The hook returns the `isLoaded` state, which can be used by components to trigger CSS transitions or animations for the fade-in effect.
    
    The introduction of this hook provides a standardized and reusable way to implement fade-in animations, enhancing the user interface's aesthetic appeal and user experience.

---
+++ california-dev/src/hooks/useFadeInOnLoad.js
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react';
+
+export const useFadeInOnLoad = (delay = 500) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [delay]);
+
+  return isLoaded;
+}
