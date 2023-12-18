861978d5c1c1a5268054c96fe3ae004b82caac85
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2023-12-18 03:15:30 -0800

    refactor: Enhance StyledSection for Responsive Layout and Typography
    This commit introduces significant enhancements to the StyledSection
    component, primarily focusing on responsive design and typographic
    consistency. The changes are tailored to ensure that the component
    adapts gracefully across various device breakpoints, especially for
    tablet and desktop layouts.

--- california-dev/src/components/sections/Home/Home.styles.js
+++ california-dev/src/components/sections/Home/Home.styles.js
@@ -5,9 +5,9 @@ export const StyledSection = styled.section(
     return css`
       display: flex;
       flex-direction: column;
-
       padding: 4rem ${media.padding.SMALL};
       align-items: center;
+      max-height: min-content;
 
       .display {
         ${typography.display.medium}
@@ -51,32 +51,155 @@ export const StyledSection = styled.section(
         .resume-btn {
           width: auto;
         }
-      }
-      @media (min-width: ${media.device.desktop}) {
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        gap: 3.2rem;
-        padding: ${media.padding.LARGE};
-        min-height: auto;
 
-        article {
+        .content-container {
           display: flex;
-          flex-direction: column;
-          gap: 1.6rem;
-        }
-        header {
-          position: relative;
-          width: 46rem;
+          gap: 6rem;
+          width: 100%;
+          align-items: center;
+          padding: 61px 61px 61px 0;
         }
 
         #photo-wrapper {
           display: flex;
           position: relative;
+          width: 20rem;
+          height: 28rem;
+        }
+
+        .profile-img {
+          z-index: 1; // !Check this
+          display: block;
+          width: inherit;
+          height: inherit;
+
+          background-color: ${palette.primary};
+          background-size: cover;
+          background-position-x: center;
+          background-image: url(${profileImg});
+          box-shadow: ${elevation.level5};
+
+          z-index: 1;
+        }
+
+        .outline-frame {
+          position: absolute;
+          width: inherit;
+          height: inherit;
+          border: 2px solid ${palette.primary};
+          left: -20px;
+          top: -20px;
+        }
+        .filled-frame {
+          position: absolute;
+          width: inherit;
+          height: inherit;
+          opacity: 0.8;
+          backdrop-filter: blur(100px);
+
+          background-color: ${palette.secondary};
+          right: -20px;
+          bottom: -20px;
+        }
+
+        .corner-svg {
+          display: block;
+          position: absolute;
+        }
+
+        .top-left {
+          top: -61px;
+          left: -61px;
+          circle {
+            fill: ${palette.primary};
+          }
+        }
+        .bottom-right {
+          bottom: -61px;
+          right: -61px;
+          circle {
+            fill: ${palette.secondary};
+          }
+        }
+
+        .zigzag-svg {
+          display: block;
+          position: absolute;
+        }
+        .zigzag-svg.secondary {
+          right: -140px;
+          top: 216px;
+          path {
+            stroke: ${palette.secondary};
+          }
+        }
+        .zigzag-svg.primary {
+          top: 16px;
+          right: -56px;
+          transform: rotate(-45deg);
+          path {
+            stroke: ${palette.primary};
+          }
+        }
+        .zigzag-svg.small {
+          top: 24px;
+          path {
+            stroke: ${palette.primary};
+          }
+          pointer-events: none;
+        }
+      }
+      @media (min-width: ${media.device.tabletLarge}) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        gap: 3.2rem;
+        padding: ${media.padding.SMALL};
+
+        article {
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          width: 100%;
+          gap: 1.6rem;
+        }
+        header {
+          position: relative;
+          width: 60rem;
+        }
+
+        .content-container {
+          width: 100%;
+          max-width: var(--desktop-max-width);
+          margin-inline: auto;
+        }
+      } // end of tablet large media query
+
+      @media (min-width: ${media.device.desktop}) {
+        padding: ${media.padding.LARGE};
+        .display {
+          ${typography.display.large}
+        }
+        .headline {
+          ${typography.headline.large}
+        }
+        p {
+          ${typography.body.xlarge}
+        }
+
+        #photo-wrapper {
           width: 36.8rem;
           height: 44.8rem;
         }
-        .content-container {
+      }
+    `;
+  }
+);
+
+
+
+        /* .content-container {
           position: relative;
           width: 100%;
           display: flex;
@@ -166,8 +289,4 @@ export const StyledSection = styled.section(
         }
         .text-wrapper {
           margin-bottom: 0;
-        }
-      } // end of desktop media query
-    `;
-  }
-);
+        } */
