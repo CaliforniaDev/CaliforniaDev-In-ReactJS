9ccd407d8779d3e9c989cec16653ae72fef253df
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-01-07 23:57:03 -0800

    Refactor navigation configuration in navConfig.js

--- california-dev/src/components/ui/Navigation/utils/navConfig.js
+++ california-dev/src/components/ui/Navigation/utils/navConfig.js
@@ -9,13 +9,43 @@ import {
 
 export const navItemsData = {
   main: [
-    { Icon: HomeIcon, id: '#home-section', name: 'Home', route: '/#home-section' },
-    { Icon: ChartIcon, id: '#workflow-section', name: 'Workflow', route: '/#workflow-section' },
-    { Icon: ImagesIcon, id: '#projects-section', name: 'Projects', route: '/#projects-section' },
-    { Icon: LightBulbIcon, id: '#skills-section', name: 'Skills', route: '/#skills-section' },
-    { Icon: EmailIcon, id: '#contact-us-section', name: 'Contact Us', route: '/#contact-us-section' },
+    {
+      Icon: HomeIcon,
+      id: '#home-section',
+      name: 'Home',
+      route: '/#home-section',
+    },
+    {
+      Icon: ChartIcon,
+      id: '#workflow-section',
+      name: 'Workflow',
+      route: '/#workflow-section',
+    },
+    {
+      Icon: ImagesIcon,
+      id: '#projects-section',
+      name: 'Projects',
+      route: '/#projects-section',
+    },
+    {
+      Icon: LightBulbIcon,
+      id: '#skills-section',
+      name: 'Skills',
+      route: '/#skills-section',
+    },
+    {
+      Icon: EmailIcon,
+      id: '#contact-us-section',
+      name: 'Contact Us',
+      route: '/#contact-us-section',
+    },
   ],
   projectDetails: [
-    { Icon: BackArrowIcon, id: '#projects-section', name: 'back', route: '/#projects-section' },
-  ]
-}
+    {
+      Icon: BackArrowIcon,
+      id: '#projects-section',
+      name: 'back',
+      route: '/#projects-section',
+    },
+  ],
+};
