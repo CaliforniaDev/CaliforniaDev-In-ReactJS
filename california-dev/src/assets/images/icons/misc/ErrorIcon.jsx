ecfdb713834e91a0a746a07d1f57d9a67426e0d6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-01-22 06:44:37 -0800

    Add new miscellaneous icons

---
+++ california-dev/src/assets/images/icons/misc/ErrorIcon.jsx
@@ -0,0 +1,16 @@
+export const ErrorIcon = ({ className }) => (
+  <svg
+    className={className}
+    width="24"
+    height="24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2Zm-1 15v-2h2v2h-2Zm0-10v6h2V7h-2Z"
+      fill="#B3261E"
+    />
+  </svg>
+);
