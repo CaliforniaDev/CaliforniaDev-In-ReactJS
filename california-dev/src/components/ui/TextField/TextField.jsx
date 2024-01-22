31f073c8b0d09cff8ae978c4e42efaeafbe2721e
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-01-22 06:48:25 -0800

    feat: enhance TextField component with error handling and support text
    Upgrade the TextField component to offer a more robust and user-friendly
    experience. This enhancement includes several key improvements:
    
    1. Integration of PropTypes for prop validation, ensuring component
       reliability and better developer experience.
    2. Addition of an 'error' prop, used to display error messages and an
       ErrorIcon when validation fails, thereby providing clear feedback to
       the user.
    3. Implementation of 'supportingText' prop to display additional
       information or context about the input field, enhancing user guidance.
    4. A new Wrapper element encapsulating the InputField and supporting text,
       improving the overall layout and design.
    5. The inclusion of default prop values for 'type' and 'multiline',
       streamlining component usage.
    
    These updates significantly enhance the TextField's functionality and
    usability, making it more adaptable to various use cases and improving
    the end-user interaction.

--- california-dev/src/components/ui/TextField/TextField.jsx
+++ california-dev/src/components/ui/TextField/TextField.jsx
@@ -1,52 +1,87 @@
-
 import React, { useState } from 'react';
-import { Input, InputField, Label } from './TextField.styles';
+import PropTypes from 'prop-types';
 
-export const TextField = React.forwardRef(({
-  label = 'Your label',
-  type = 'text',
-  multiline = false,
-  value,
-  onChange,
-  id,
-  onKeyDown,
-  ...props
-}, ref) => {
-  const [isFocused, setIsFocused] = useState(false);
-  function handleFocus() {
-    setIsFocused(true);
+import { Input, InputField, Label, Wrapper } from './TextField.styles';
+import { ErrorIcon } from 'assets/images/icons/misc';
+// * Add Delete button to clear text field
+// import { DeleteIcon } from 'assets/images/icons/misc';
+
+export const TextField = React.forwardRef(
+  (
+    {
+      label = 'Your label',
+      type = 'text',
+      multiline = false,
+      value,
+      onChange,
+      id,
+      onKeyDown,
+      error,
+      supportingText,
+      ...props
+    },
+    ref
+  ) => {
+    const [isFocused, setIsFocused] = useState(false);
+    function handleFocus() {
+      setIsFocused(true);
+    }
+
+    function handleBlur() {
+      setIsFocused(false);
+    }
+
+    const InputOrTextarea = multiline ? 'textarea' : 'input';
+
+    return (
+      <Wrapper>
+        <InputField $isFocused={isFocused}>
+          <Input
+            id={id}
+            ref={ref}
+            as={InputOrTextarea}
+            type={type}
+            value={value}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            $isFocused={isFocused}
+            $multiline={multiline}
+            $error={error}
+            {...props}
+          />
+          <Label
+            className="label"
+            htmlFor={id}
+            $isFocused={isFocused}
+            $isPopulated={value !== ''}
+            $error={error}
+          >
+            {label}
+          </Label>
+          {error && <ErrorIcon className="trailing-icon error" />}
+        </InputField>
+        {supportingText && !error && <p className="supporting-text">{supportingText}</p>}
+        {error && <p className="supporting-text error">{error}</p>}
+      </Wrapper>
+    );
   }
-
-  function handleBlur() {
-    setIsFocused(false);
-  }
-
-  const InputOrTextarea = multiline ? 'textarea' : 'input';
-
-  return (
-    <InputField $isFocused={isFocused}>
-      <Input
-        id={id}
-        ref={ref}
-        as={InputOrTextarea}
-        type={type}
-        value={value}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        $isFocused={isFocused}
-        $multiline={multiline}
-        {...props}
-      />
-
-      <Label htmlFor={id} $isFocused={isFocused} $isPopulated={value !== ''}>
-        {label}
-      </Label>
-    </InputField>
-  );
-});
+);
 
 TextField.displayName = 'TextField';
 
+TextField.defaultProps = {
+  type: 'text',
+  multiline: false,
+};
 
+TextField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  id: PropTypes.string,
+  onKeyDown: PropTypes.func,
+  multiline: PropTypes.bool,
+};
--- california-dev/src/components/ui/TextField/TextField.styles.js
+++ california-dev/src/components/ui/TextField/TextField.styles.js
@@ -8,7 +8,7 @@ const afterPseudoElementStyling = css`
     left: 0;
     right: 0;
     bottom: 0;
-    
+
     border-top-left-radius: 4px;
     border-top-right-radius: 4px;
 
@@ -21,12 +21,46 @@ const afterPseudoElementStyling = css`
   }
 `;
 
+export const Wrapper = styled.div(({ theme: { palette, typography } }) => {
+  const TEXT_COLOR = palette.onSurfaceVariant;
+  const ERROR_COLOR = palette.error;
+  const ICON_COLOR = palette.onSurfaceVariant;
+  const ERROR_ICON_COLOR = palette.error;
+  return css`
+    .supporting-text {
+      padding: 0.4rem 1.6rem 0 1.6rem;
+      color: ${TEXT_COLOR};
+      ${typography.body.small}
+    }
+    .supporting-text.error {
+      color: ${ERROR_COLOR};
+    }
+
+    .trailing-icon {
+      position: absolute;
+      top: 50%;
+      right: 16px;
+      transform: translateY(-50%);
+      cursor: pointer;
+      path {
+        fill: ${ICON_COLOR};
+      }
+    }
+
+    .trailing-icon.error {
+      pointer-events: none;
+      path {
+        fill: ${ERROR_ICON_COLOR};
+      }
+    }
+  `;
+});
+
 export const InputField = styled.div(({ theme: { palette } }) => {
   return css`
     position: relative;
     display: block;
     width: 100%;
-    
     ${afterPseudoElementStyling}
     &:hover::after {
       background-color: ${palette.onSurface};
@@ -36,52 +70,87 @@ export const InputField = styled.div(({ theme: { palette } }) => {
 });
 
 export const Input = styled.input(
-  ({ theme: { palette, typography }, $isFocused, $multiline }) => css`
-    width: 100%;
-    position: relative;
-    height: ${$multiline ? '168px' : '56px'};
-    padding: 2.4rem 1.6rem 0.8rem 1.6rem;
+  ({ theme: { palette, typography }, $isFocused, $multiline, $error }) => {
+    const BORDER_WIDTH = $isFocused ? '2px' : '1px';
+    const BORDER_COLOR = $error ? palette.error : palette.onSurfaceVariant;
+    const ACTIVE_COLOR = $error ? palette.error : palette.primary;
+    const HOVER_COLOR = $error ? palette.onErrorContainer : palette.onSurface;
+    const CARET_COLOR = $error ? palette.error : palette.primary;
+    return css`
+      width: 100%;
+      position: relative;
+      height: ${$multiline ? '168px' : '56px'};
+      padding: 2.4rem 1.6rem 0.8rem 1.6rem;
 
-    border: none;
-    /** During Focus State border style changes */
-    border-bottom: ${$isFocused ? '2px' : '1px'} solid ${palette.onSurfaceVariant}; 
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
-    
-    background-color: ${palette.surfaceContainerHighest};
-    color: ${palette.onSurface};
-    caret-color: ${palette.primary};
+      border: none;
+      /** During Focus State border style changes */
+      border-bottom: ${BORDER_WIDTH} solid ${BORDER_COLOR};
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
 
-    transition:
-      background-color 0.3s ease-in-out,
-      box-shadow 0.3s ease-in-out,
-      border 0.3s ease-in-out;
+      background-color: ${palette.surfaceContainerHighest};
+      color: ${palette.onSurface};
+      caret-color: ${CARET_COLOR};
 
+      transition:
+        background-color 0.3s ease-in-out,
+        box-shadow 0.3s ease-in-out,
+        border 0.3s ease-in-out;
 
-    ${typography.body.large}
+      ${typography.body.large}
 
-    &:focus {
-      outline: none;
-      border-color: ${palette.primary};
-    }
-  `
+      &:focus {
+        outline: none;
+        border-color: ${ACTIVE_COLOR};
+      }
+
+      &:hover {
+        border-color: ${HOVER_COLOR};
+      }
+
+      &:hover ~ .trailing-icon.error {
+        path {
+          fill: ${palette.onErrorContainer};
+        }
+      }
+
+      &:hover ~ ${Label} {
+        color: ${$error && palette.onErrorContainer};
+      }
+    `;
+  }
 );
 
 export const Label = styled.label(
-  ({ theme: { palette, typography }, $isFocused, $isPopulated }) => {
+  ({ theme: { palette, typography }, $isFocused, $isPopulated, $error }) => {
+    const PADDING = $isFocused || $isPopulated ? '8px' : '16px';
+
+    const FONT_SIZE =
+      $isFocused || $isPopulated
+        ? typography.body.small
+        : typography.body.large;
+
+    const COLOR = $error
+      ? palette.error
+      : $isFocused
+      ? palette.primary
+      : palette.onSurfaceVariant;
+
     return css`
       position: absolute;
       z-index: 1;
       left: 16px;
       right: 16px;
       text-align: left;
-      padding-top: ${$isFocused || $isPopulated ? '8px' : '16px'};
-      font-size: ${$isFocused || $isPopulated
-        ? typography.body.small
-        : typography.body.large};
-      color: ${$isFocused ? palette.primary : palette.onSurfaceVariant};
+      padding-top: ${PADDING};
+      font-size: ${FONT_SIZE};
+      color: ${COLOR};
       transition: all 0.3s ease-in-out;
       pointer-events: none;
+
+      &:hover {
+        color: green;
+      }
     `;
   }
 );
