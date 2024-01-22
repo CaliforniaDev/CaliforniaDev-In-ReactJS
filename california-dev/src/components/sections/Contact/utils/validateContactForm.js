d4ac3002952fca9f762f9c507352c7252feafa5d
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-01-22 06:46:52 -0800

    feat: enhance validateContactForm with message validation
    This update includes a new validation check for the 'message' field,
    ensuring it is not left empty. Additionally, the requirement for either an
    email or phone number has been altered to make the email field mandatory.

--- california-dev/src/components/sections/Contact/utils/validateContactForm.js
+++ california-dev/src/components/sections/Contact/utils/validateContactForm.js
@@ -7,25 +7,32 @@
 
 export function validateContactForm(formData) {
   const errors = {};
-  const { firstName, lastName, email, phoneNumber, inquiryType } = formData;
+  const { firstName, lastName, email, phoneNumber, inquiryType, message } =
+    formData;
+  console.log('formData: ', !message);
 
   if (!firstName) errors.firstName = 'First name is required';
   if (!lastName) errors.lastName = 'Last name is required';
 
-  if (!email && !phoneNumber) {
-    errors.email = 'Email or phone number is required';
-    errors.phoneNumber = 'Email or phone number is required';
-    return errors;
-  }
+  if (!email) errors.email = 'Email is required';
+
   // Check if email is valid
   if (email && !/^\S+@\S+\.\S+$/.test(email))
     errors.email = 'Email address is invalid';
-  
+
   // Check if phone number is valid
   if (phoneNumber && !/^[0-9]+$/.test(phoneNumber))
     errors.phoneNumber = 'Phone number is invalid';
 
   if (!inquiryType) errors.inquiryType = 'Inquiry type is required';
 
+  if (!message) errors.message = 'Message is required';
+
   return errors;
 }
+
+
+// if (!email && !phoneNumber) {
+  //   errors.email = 'Email or phone number is required';
+  //   errors.phoneNumber = 'Email or phone number is required';
+  // }
